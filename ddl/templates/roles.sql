7213b756e60f0c6eb6b3a16b8dcf30df918d1b9a
Ray Miller <rm7@sanger.ac.uk>
2012-05-09 16:52:18 +0100

    Update database schema for consistency, change QC components to better model the relation between sequencing reads and sequencing project plate/well

--- ddl/templates/roles.sql
+++ ddl/templates/roles.sql
@@ -1,10 +1,9 @@
-CREATE ROLE "[% admin_role     %]" NOLOGIN NOINHERIT;
-CREATE ROLE "[% rw_role        %]" NOLOGIN NOINHERIT;
-CREATE ROLE "[% ro_role        %]" NOLOGIN NOINHERIT;
-CREATE ROLE "[% webapp_role    %]" WITH ENCRYPTED PASSWORD '[% webapp_passwd    %]' LOGIN NOINHERIT IN ROLE "[% ro_role %]";
-CREATE ROLE "[% webapp_ro_role %]" WITH ENCRYPTED PASSWORD '[% webapp_ro_passwd %]' LOGIN INHERIT IN ROLE "[% ro_role %]";
-CREATE ROLE "[% task_role      %]" WITH ENCRYPTED PASSWORD '[% task_passwd      %]' LOGIN INHERIT IN ROLE "[% rw_role %]";
-CREATE ROLE "[% test_role      %]" WITH ENCRYPTED PASSWORD '[% test_passwd      %]' LOGIN INHERIT IN ROLE "[% rw_role %]";
+CREATE ROLE "[% admin_role      %]" NOLOGIN NOINHERIT;
+CREATE ROLE "[% rw_role         %]" NOLOGIN NOINHERIT;
+CREATE ROLE "[% ro_role         %]" NOLOGIN NOINHERIT;
+CREATE ROLE "[% webapp_role     %]" WITH ENCRYPTED PASSWORD '[% webapp_passwd    %]' LOGIN NOINHERIT IN ROLE "[% ro_role %]";
+CREATE ROLE "[% webapp_ro_role  %]" WITH ENCRYPTED PASSWORD '[% webapp_ro_passwd %]' LOGIN INHERIT IN ROLE "[% ro_role %]";
+CREATE ROLE "[% task_role       %]" WITH ENCRYPTED PASSWORD '[% task_passwd      %]' LOGIN INHERIT IN ROLE "[% rw_role %]";
 
 [%- FOR u IN webapp_users %]
 GRANT "[% rw_role %]" TO "[% u %]";
--- ddl/templates/versions/1/fixtures.sql
+++ ddl/templates/versions/1/fixtures.sql
@@ -1,2 +1,6 @@
-INSERT INTO roles (role_name)
-VALUES ('admin'), ('edit'),('read');
+
+INSERT INTO schema_versions (version) VALUES (1);
+
+INSERT INTO roles (name)
+VALUES ('admin'),('edit'),('read');
+
