a9a017be8c4234e414e8e804e83bfb91905ab0f1
Ray Miller <rm7@sanger.ac.uk>
2012-05-15 16:10:49 +0100

    Create database (as well as application) test user

--- t/fixtures/10-users-roles.sql
+++ t/fixtures/10-users-roles.sql
@@ -8,3 +8,9 @@ INSERT INTO user_role (user_id,role_id)
 SELECT users.id, roles.id
 FROM users, roles
 WHERE users.name = 'test_user@example.org' AND roles.name = 'edit';
+
+SET ROLE lims2_test_admin;
+DROP ROLE IF EXISTS "test_user@example.org";
+CREATE ROLE "test_user@example.org" WITH NOLOGIN INHERIT IN ROLE lims2_test_rw;
+GRANT "test_user@example.org" TO lims2_test_webapp;
+RESET ROLE;
