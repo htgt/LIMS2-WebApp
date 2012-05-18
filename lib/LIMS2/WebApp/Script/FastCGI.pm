7aaaf36747e82a424bed850fa36cae5372fc62a3
Ray Miller <rm7@sanger.ac.uk>
2012-05-18 13:22:28 +0100

    Initial check-in of FastCGI script

---
+++ lib/LIMS2/WebApp/Script/FastCGI.pm
@@ -0,0 +1,17 @@
+package LIMS2::WebApp::Script::FastCGI;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose;
+use namespace::autoclean;
+
+extends qw( Catalyst::Script::FastCGI );
+
+has '+manager' => (
+    default => 'FCGI::ProcManager::Constrained'
+);
+
+__PACKAGE__->meta->make_immutable;
+
+1;
