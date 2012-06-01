04579caebfb4973a274a416fe92bd0814b05e7c5
Ray Miller <rm7@sanger.ac.uk>
2012-06-01 10:51:42 +0100

    Test fixtures for designs and related data

--- t/fixtures/20-constrained-vocab.sql
+++ t/fixtures/20-constrained-vocab.sql
@@ -13,25 +13,3 @@ VALUES ('conditional'), ('deletion'), ('insertion'), ('artificial-intron'), ('in
 
 INSERT INTO design_oligo_types(id)
 VALUES ('G5'), ('U5'), ('U3'), ('D5'), ('D3'), ('G3');
-
-INSERT INTO design_comment_categories(name)
- VALUES ('Alternative variant not targeted'),
-        ('NMD rescue'),
-        ('Possible reinitiation'),
-        ('Non protein coding locus'),
-        ('Conserved elements'),
-        ('Recovery design'),
-        ('No NMD'),
-        ('Other'),
-        ('No BACs available'),
-        ('Warning!'),
-        ('Upstream domain unaffected'),
-        ('Overlapping locus');      
-
-INSERT INTO genotyping_primer_types(id)
-VALUES ('GF1'), ('GF2'), ('GF3'), ('GF4'),
-       ('GR1'), ('GR2'), ('GR3'), ('GR4'),
-       ('LF1'), ('LF2'), ('LF3'),
-       ('LR1'), ('LR2'), ('LR3'),
-       ('PNFLR1'), ('PNFLR2'), ('PNFLR3'),
-       ('EX3'), ('EX32'), ('EX5'), ('EX52');
