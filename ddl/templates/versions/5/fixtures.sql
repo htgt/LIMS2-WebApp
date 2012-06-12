d8e3a0277750a5dfb93153bfa003867bde15f764
Ray Miller <rm7@sanger.ac.uk>
2012-06-12 16:32:43 +0100

    Add process types (and descriptions) to fixtures

--- ddl/templates/versions/5/fixtures.sql
+++ ddl/templates/versions/5/fixtures.sql
@@ -1,3 +1,12 @@
-INSERT INTO schema_versions(version) VALUES(5);
+INSERT INTO schema_versions(version)
+VALUES (5);
 
-INSERT INTO plate_types (id) VALUES ('DESIGN','PCS','PGS','DNA','EP','EPD','FP','CREBAC');
+INSERT INTO plate_types(id)
+VALUES ('DESIGN'),('PCS'),('PGS'),('DNA'),('EP'),('EPD'),('FP'),('CREBAC');
+
+INSERT INTO process_types(id,description)
+VALUES ('create_di', 'Create design instance'),
+       ('cre_bac_recom', 'Cre/BAC recombineering'),
+       ('int_recom', 'Intermediate recombineering'),
+       ('2w_gateway', 'Two-way gateway' ),
+       ('3w_gateway', 'Three-way gateway' );
