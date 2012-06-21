b6181ea909227108f2a30d08dbb758a0c3cae6b0
Sajith Perera <sajith.perera@gmail.com>
2012-06-21 12:38:56 +0100

    add new constrained data to test fixtures

--- t/fixtures/20-constrained-vocab.sql
+++ t/fixtures/20-constrained-vocab.sql
@@ -13,3 +13,23 @@ VALUES ('conditional'), ('deletion'), ('insertion'), ('artificial-intron'), ('in
 
 INSERT INTO design_oligo_types(id)
 VALUES ('G5'), ('U5'), ('U3'), ('D5'), ('D3'), ('G3');
+
+INSERT INTO process_types(id,description)
+VALUES ('create_di', 'Create design instance'),
+       ('cre_bac_recom', 'Cre/BAC recombineering'),
+       ('int_recom', 'Intermediate recombineering'),
+       ('2w_gateway', 'Two-way gateway'),
+       ('3w_gateway', 'Three-way gateway'),
+       ('rearray', 'rearray wells'),
+       ('recombinase', 'apply recombinase');
+
+INSERT INTO recombinases(id)
+VALUES ('Cre'),('Flp'),('Dre');
+
+INSERT INTO plate_types(id,description)
+VALUES ('DESIGN', 'Design Instances'),
+       ('INT', 'Intermediate Vectors' ),
+       ('POSTINT', 'Post-intermediate Vectors' ),
+       ('FINAL', 'Final Vectors' ),
+       ('CREBAC', 'Cre/BAC Vectors' ),
+       ('DNA', 'DNA QC' );
