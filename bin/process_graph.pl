ce571900f544b430d8352aa68a9497b80a320a66
Ray Miller <rm7@sanger.ac.uk>
2012-06-27 16:53:11 +0100

    Initial check-in of a driver script for the process graph module

---
+++ bin/process_graph.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings FATAL => 'all';
+
+use LIMS2::Model;
+use LIMS2::Model::ProcessGraph;
+use Log::Log4perl qw( :levels );
+use Getopt::Long;
+
+my $log_level = $WARN;
+
+GetOptions(
+    trace       => sub { $log_level = $TRACE },
+    debug       => sub { $log_level = $DEBUG },
+    verbose     => sub { $log_level = $INFO },
+    'output=s'  => \my $output_file,
+) and @ARGV == 2
+    or die "Usage: $0 PLATE_NAME WELL_NAME\n";
+
+Log::Log4perl->easy_init(
+    {
+        level  => $log_level,
+        layout => '%m%n'
+    }
+);
+
+my ( $plate_name, $well_name ) = @ARGV;
+
+my $model = LIMS2::Model->new( user => 'tasks' );
+
+my $well = $model->retrieve_well( { plate_name => $plate_name, well_name => $well_name } );
+
+my $graph = LIMS2::Model::ProcessGraph->new( $well );
+
+$graph->render( output_file => $output_file );
