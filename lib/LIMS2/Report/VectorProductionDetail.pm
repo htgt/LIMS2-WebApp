cd07f5b303dabe8f74755cd449c0830538ee7138
Ray Miller <rm7@sanger.ac.uk>
2012-07-05 16:00:25 +0100

    Add vector production detail report

---
+++ lib/LIMS2/Report/VectorProductionDetail.pm
@@ -0,0 +1,62 @@
+package LIMS2::Report::VectorProductionDetail;
+
+use Moose;
+use DateTime;
+use List::MoreUtils qw( uniq );
+use namespace::autoclean;
+
+with qw( LIMS2::Role::ReportGenerator );
+
+sub _build_name {
+    my $dt = DateTime->now();
+    return 'Vector Production Detail ' . $dt->ymd;    
+}
+
+sub _build_columns {
+    return [
+        "MGI Accession Id", "Marker Symbol", "Plate", "Well", "Design", "Cassette", "Backbone", "Recombinase", "Accepted?"
+    ];
+}
+
+sub iterator {
+    my ( $self, $model ) = @_;
+
+    my $final_vectors_rs = $model->schema->resultset( 'Well' )->search(
+        {
+            'plate.type_id' => 'FINAL'
+        },
+        {
+            join     => 'plate',
+            prefetch => 'well_accepted_override',
+            order_by => { -asc => 'me.created_at' }
+        }
+    );
+
+    return Iterator::Simple::iter(
+        sub {
+            my $well = $final_vectors_rs->next
+                or return;
+            my $design = $well->design;
+            my @mgi_accessions = map { $_->gene_id } $design->genes;    
+            my @marker_symbols = uniq map { $_->{marker_symbol} }
+                map { @{ $model->search_genes( { gene => $_ } ) } } @mgi_accessions;
+            return [
+                join( '/', @mgi_accessions ),
+                join( '/', @marker_symbols ),
+                $well->plate->name,
+                $well->name,
+                $design->id,
+                $well->cassette,
+                $well->backbone,
+                join( '/', @{$well->recombinases} ),
+                ( $well->is_accepted ? 'yes': 'no' )
+            ];
+        }
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
