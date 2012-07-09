31283a7a05079f487151c24f34f5846f086c622d
Sajith Perera <sajith.perera@gmail.com>
2012-07-09 13:12:33 +0100

    add reports for QcRun and QcRun Summary

---
+++ lib/LIMS2/Report/QcRunSummary.pm
@@ -0,0 +1,75 @@
+package LIMS2::Report::QcRunSummary;
+
+use Moose;
+use LIMS2::Model::Util::QCResults qw( retrieve_qc_run_summary_results );
+use namespace::autoclean;
+
+with qw( LIMS2::Role::ReportGenerator );
+
+has qc_run_id => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1
+);
+
+has qc_run => (
+    is         => 'ro',
+    isa        => 'LIMS2::Model::Schema::Result::QcRun',
+    lazy_build => 1,
+);
+
+sub _build_qc_run {
+    my $self = shift;
+
+    return $self->model->retrieve( 'QcRun' => { id => $self->qc_run_id } );
+}
+
+sub _build_name {
+    my $self = shift;
+
+    my $id = substr( $self->qc_run_id, 0, 8 );
+    return $id . ' QC Run Report ';
+}
+
+sub _build_columns {
+    return [
+        qw(
+              design_id
+              marker_symbol
+              plate_name
+              well_name
+              well_name_384
+              pass
+              valid_primers
+        )
+    ];
+}
+
+sub iterator {
+    my ( $self ) = @_;
+
+    my $qc_results = retrieve_qc_run_summary_results( $self->qc_run );
+
+    my $qc_result = shift @{ $qc_results };
+
+    return Iterator::Simple::iter(
+        sub {
+            return unless $qc_result;
+
+            my @data;
+            for my $column ( @{ $self->columns } ) {
+                my $datum = defined $qc_result->{$column} ? $qc_result->{$column} : '';
+                push @data, $datum;
+            }
+
+            $qc_result = shift @{ $qc_results };
+            return \@data;
+        }
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
