b53be40d49346fe8b9e5f7f833edac18fe097241
Sajith Perera <sajith.perera@gmail.com>
2012-07-17 12:48:35 +0100

    add new up and fixtures sql for new db version

---
+++ ddl/templates/versions/6/fixtures.sql
@@ -0,0 +1,36 @@
+INSERT INTO plate_types(id,description)
+VALUES ('EP', 'Electroporation'),
+       ('EP_PICK', 'ES Cells' ),
+       ('XEP', 'Electroporation With Recombinase Applied' ),
+       ('XEP_PICK', 'ES Cells With Recombinase Applied' ),
+       ('XEP_POOL', 'ES Cells Backup Vial' ),
+       ('SEP', 'Second Allele Electroporation' ),
+       ('SEP_PICK', 'Second Allele ES Cells' ),
+       ('SEP_POOL', 'Second Allele Backup Vial' ),
+       ('SFP', 'Second Allele Freezer Plates' ),
+       ('FP', 'Freezer Plates' );
+
+INSERT INTO process_types(id,description)
+VALUES ('clone_pick', 'Pick from EP plate'),
+       ('clone_pool', 'Pool es cells into backup vial'),
+       ('first_electroporation', 'First (standard) electroporation'),
+       ('second_electroporation', 'Second electroporation in double targetted cells'),
+       ('freeze', 'Create freezer plate well');
+
+INSERT INTO primer_band_types(id)
+VALUES ('gr1'),
+       ('gr2'),
+       ('gr3'),
+       ('gr4'),
+       ('gf1'),
+       ('gf2'),
+       ('gf3'),
+       ('gf4'),
+       ('tr_pcr');
+
+INSERT INTO colony_types(id)
+VALUES ('blue_colonies'), 
+       ('white_colonies'),
+       ('picked_colonies'),
+       ('total_colonies'),
+       ('remaining_unstained_colonies'); 
---
+++ ddl/templates/versions/6/up.sql
@@ -0,0 +1,40 @@
+CREATE TABLE process_electroporation (
+       process_id      INTEGER PRIMARY KEY REFERENCES processes(id),
+       cell_line       TEXT NOT NULL
+);
+GRANT SELECT ON process_electroporation TO "[% ro_role %]";
+GRANT SELECT, INSERT, UPDATE, DELETE ON process_electroporation TO "[% rw_role %]";
+
+CREATE TABLE colony_types (
+       id TEXT PRIMARY KEY
+);
+GRANT SELECT ON colony_types TO "[% ro_role %]";
+GRANT SELECT, INSERT, UPDATE, DELETE ON colony_types TO "[% rw_role %]";
+
+CREATE TABLE well_colony_picks (
+       well_id                INTEGER NOT NULL REFERENCES wells(id),
+       colony_type_id         TEXT NOT NULL REFERENCES colony_types(id),
+       count                  INTEGER NOT NULL,
+       created_at             TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       created_by_id          INTEGER NOT NULL REFERENCES users(id),
+       PRIMARY KEY(well_id, colony_type_id)
+);
+GRANT SELECT ON well_colony_picks TO "[% ro_role %]";
+GRANT SELECT, INSERT, UPDATE, DELETE ON well_colony_picks TO "[% rw_role %]";
+
+CREATE TABLE primer_band_types (
+       id TEXT PRIMARY KEY
+);
+GRANT SELECT ON primer_band_types TO "[% ro_role %]";
+GRANT SELECT, INSERT, UPDATE, DELETE ON primer_band_types TO "[% rw_role %]";
+
+CREATE TABLE well_primer_bands (
+       well_id                 INTEGER NOT NULL REFERENCES wells(id),
+       primer_band_type_id     TEXT NOT NULL REFERENCES primer_band_types(id),
+       pass                    BOOLEAN DEFAULT TRUE,
+       created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+       created_by_id           INTEGER NOT NULL REFERENCES users(id),
+       PRIMARY KEY(well_id, primer_band_type_id)
+);
+GRANT SELECT ON well_primer_bands TO "[% ro_role %]";
+GRANT SELECT, INSERT, UPDATE, DELETE ON well_primer_bands TO "[% rw_role %]";
