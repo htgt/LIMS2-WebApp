b376140e5c9ea7ce1d8c06bd8092ef0ae5f0d91d
Ray Miller <rm7@sanger.ac.uk>
2012-07-19 10:40:23 +0100

    perltidy

--- lib/LIMS2/Report/PlateList.pm
+++ lib/LIMS2/Report/PlateList.pm
@@ -16,37 +16,35 @@ has plate_type => (
 sub _build_name {
     if ( $self->plate_type ) {
         return $self->plate_type . ' Plate List';
-    }    
-    return 'Plate List';    
+    }
+    return 'Plate List';
 }
 
 sub _build_columns {
-    return [
-        "Plate Name", "Plate Type", "Description", "Created By", "Created At"
-    ];
+    return [ "Plate Name", "Plate Type", "Description", "Created By", "Created At" ];
 }
 
 sub iterator {
-    my ( $self ) = @_;
+    my ($self) = @_;
 
-    my %search_params;    
-    
+    my %search_params;
+
     if ( $self->plate_type and $self->plate_type ne '-' ) {
         $search_params{'me.type_id'} = $self->plate_type;
     }
 
-    my $plate_rs = $self->model->schema->resultset( 'Plate' )->search(
+    my $plate_rs = $self->model->schema->resultset('Plate')->search(
         \%search_params,
-        {
-            order_by => { -desc => 'created_at' },
-            prefetch => [ 'created_by' ]
+        {   order_by => { -desc => 'created_at' },
+            prefetch => ['created_by']
         }
     );
 
     return Iterator::Simple::iter sub {
         my $plate = $plate_rs->next
             or return;
-        return [ $plate->name, $plate->type_id, $plate->description, $plate->created_by->name, $plate->created_at->ymd ];        
+        return [ $plate->name, $plate->type_id, $plate->description, $plate->created_by->name,
+            $plate->created_at->ymd ];
     };
 }
 
