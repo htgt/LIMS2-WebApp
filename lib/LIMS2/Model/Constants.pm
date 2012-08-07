52b9e20807fad92a1e94e84996b65a3a6e88fb40
Sajith Perera <sajith.perera@gmail.com>
2012-08-07 09:25:43 +0100

    add process plate types hash to constants module

--- lib/LIMS2/Model/Constants.pm
+++ lib/LIMS2/Model/Constants.pm
@@ -8,11 +8,26 @@ use base 'Exporter';
 use Const::Fast;
 
 BEGIN {
-    our @EXPORT_OK   = qw( $DEFAULT_ASSEMBLY );
+    our @EXPORT_OK   = qw( $DEFAULT_ASSEMBLY %PROCESS_PLATE_TYPES );
     our %EXPORT_TAGS = ();
 }
 
 const our $DEFAULT_ASSEMBLY => 'NCBIM37';
+
+const our %PROCESS_PLATE_TYPES => (
+    create_di              => [ qw( DESIGN ) ],
+    int_recom              => [ qw( INT ) ],
+    cre_bac_recom          => [ qw( INT ) ],
+    '2w_gateway'           => [ qw( POSTINT FINAL ) ],
+    '3w_gateway'           => [ qw( POSTINT FINAL ) ],
+    dna_prep               => [ qw( DNA ) ],
+    recombinase            => [ qw( FINAL XEP POSTINT ) ],
+    first_electroporation  => [ qw( EP ) ],
+    second_electroporation => [ qw( SEP ) ],
+    clone_pick             => [ qw( EP_PICK SEP_PICK XEP_PICK ) ],
+    clone_pool             => [ qw( SEP_POOL XEP_POOL ) ],
+    freeze                 => [ qw( FP SFP ) ],
+);
 
 1;
 
