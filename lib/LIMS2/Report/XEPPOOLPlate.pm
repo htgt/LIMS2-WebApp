6cec99888ff82845108194eb07aaddc9dc42d6f5
Sajith Perera <sajith.perera@gmail.com>
2012-08-13 14:42:33 +0100

    POOL plate types need seperate report classes

---
+++ lib/LIMS2/Report/XEPPOOLPlate.pm
@@ -0,0 +1,54 @@
+package LIMS2::Report::XEPPOOLPlate;
+
+use Moose;
+use namespace::autoclean;
+
+extends qw( LIMS2::ReportGenerator::Plate::SingleTargeted );
+
+override plate_types => sub {
+    return [ 'XEP_POOL' ];
+};
+
+override _build_name => sub {
+    my $self = shift;
+
+    return 'XEP Pool Plate ' . $self->plate_name;
+};
+
+# Basic columns, will need to add more
+override _build_columns => sub {
+    my $self = shift;
+
+    return [
+        $self->base_columns,
+    ];
+};
+
+override iterator => sub {
+    my $self = shift;
+
+    my $wells_rs = $self->plate->search_related(
+        wells => {},
+        {
+            prefetch => [
+                'well_accepted_override', 'well_qc_sequencing_result'
+            ],
+            order_by => { -asc => 'me.name' }
+        }
+    );
+
+    return Iterator::Simple::iter sub {
+        my $well = $wells_rs->next
+            or return;
+
+        return [
+            $self->base_data( $well ),
+        ];
+    };
+};
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
