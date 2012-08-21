f743f1378b1fbbc32f3b61357f23e4daa39002ce
Sajith Perera <sajith.perera@gmail.com>
2012-08-21 07:20:47 +0100

    initial commit of create plate util module

---
+++ lib/LIMS2/Model/Util/CreatePlate.pm
@@ -0,0 +1,21 @@
+package LIMS2::Model::Util::CreatePlate;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Sub::Exporter -setup => {
+    exports => [
+        qw(
+            
+          )
+    ]
+};
+
+use Log::Log4perl qw( :easy );
+use List::MoreUtils qw( uniq );
+use LIMS2::Exception;
+
+
+1;
+
+__END__
