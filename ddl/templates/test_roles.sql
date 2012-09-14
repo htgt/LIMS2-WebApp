d0a1db04d3acd41965827d8316b6a8762c104bc0
Sajith Perera <sajith.perera@gmail.com>
2012-09-14 14:49:18 +0100

    add grants to allow new db to have tests run against it

--- ddl/templates/test_roles.sql
+++ ddl/templates/test_roles.sql
@@ -3,3 +3,10 @@ CREATE ROLE "[% test_role       %]" WITH ENCRYPTED PASSWORD '[% test_passwd     
 
 GRANT "[% test_admin_role %]" TO "[% test_role       %]";
 GRANT "[% admin_role      %]" TO "[% test_admin_role %]";
+GRANT "[% ro_role         %]" TO "[% webapp_role     %]";
+
+GRANT "test_user@example.org" TO "[% webapp_role  %]";
+GRANT "admin_user@example.org" TO "[% webapp_role %]";
+
+GRANT "[% rw_role %]" TO "test_user@example.org";
+GRANT "[% rw_role %]" TO "admin_user@example.org";
