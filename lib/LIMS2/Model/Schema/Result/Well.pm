e0d349a3f43eaffb1c9f33ca37438ecdd8fa79fb
David Parry-Smith <david.parry-smith@sanger.ac.uk>
2013-01-24 09:56:52 +0000

    Added design_id datum item to model data retrieval for Genotyping QC Results View
    This is the retrieval method that uses the model (DBIx Class) for data
    retrieval.

--- lib/LIMS2/Model/Schema/Result/Well.pm
+++ lib/LIMS2/Model/Schema/Result/Well.pm
@@ -631,6 +631,7 @@ sub all_genotyping_qc_data{
     #TODO: translate gene_id to a symbolic string
     my ($design) = $self->designs;
     $datum->{gene_name} = $design->genes->first->gene_id if $design;
+    $datum->{design_id} = $design->id;
 
 	$datum->{chromosome_fail} = $self->well_chromosome_fail ? $self->well_chromosome_fail->result
 		                                                    : undef;
