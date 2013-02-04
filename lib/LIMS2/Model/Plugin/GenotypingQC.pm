1d4d34a66b4c1bc93c251d9a090f6cf7cf298621
David Parry-Smith <david.parry-smith@sanger.ac.uk>
2013-02-04 09:56:13 +0000

    Updated comment to use update_or_create_well_primer_band method

--- lib/LIMS2/Model/Plugin/GenotypingQC.pm
+++ lib/LIMS2/Model/Plugin/GenotypingQC.pm
@@ -131,6 +131,7 @@ sub update_genotyping_qc_data{
 				die "Invalid data \"$value\" provided for well ".$datum->{well_name}." $primer" unless $value eq "yes";
 
 				# FIXME: need an update or create method
+                # update_or_create_well_primer_band now implemented and this code should be updated to use it
 				$self->create_well_primer_bands({
 					well_id          => $well->id,
 					primer_band_type => $primer,
