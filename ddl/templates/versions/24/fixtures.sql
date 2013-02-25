f253babbb7d45ae97d0517fa73c44f15864ffb7c
as28 <as28@sanger.ac.uk>
2013-02-25 15:07:08 +0000

    Final pick report and tests in progress

--- ddl/templates/versions/24/fixtures.sql
+++ ddl/templates/versions/24/fixtures.sql
@@ -1,6 +1,9 @@
 INSERT INTO schema_versions(version)
 VALUES (24);
 
-INSERT INTO plate_types 
+INSERT INTO process_types (id,description) 
+VALUES ('final_pick','Create Final Pick plate');
+
+INSERT INTO plate_types (id,description)
 VALUES ('FINAL_PICK', 'Final vector picks');
 
