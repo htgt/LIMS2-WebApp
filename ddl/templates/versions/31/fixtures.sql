deaf414507c61cb729d201ed310659d6bd5bfe0f
Sajith Perera <sajith.perera@gmail.com>
2013-05-28 15:19:27 +0100

    move to version 31, not 30

--- ddl/templates/versions/31/fixtures.sql
+++ ddl/templates/versions/31/fixtures.sql
@@ -1,4 +1,4 @@
 INSERT INTO crispr_loci_types(id) VALUES ('Exon'), ('Intron'), ('Intergenic');
-INSERT INTO schema_versions(version) VALUES (30);
+INSERT INTO schema_versions(version) VALUES (31);
 INSERT INTO plate_types(id, description) VALUES ('CRISPR', 'Crispr Plate');
 INSERT INTO process_types ( id, description  ) VALUES ( 'create_crispr', 'Create crispr' );
