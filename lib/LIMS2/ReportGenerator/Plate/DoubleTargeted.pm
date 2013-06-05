86e90068c94d1ee75dd2e317ad17b01a8808d8b2
as28 <as28@sanger.ac.uk>
2013-06-05 16:27:18 +0100

    Fixes for some FINAL_PICK issues

--- lib/LIMS2/ReportGenerator/Plate/DoubleTargeted.pm
+++ lib/LIMS2/ReportGenerator/Plate/DoubleTargeted.pm
@@ -12,7 +12,7 @@ extends qw( LIMS2::ReportGenerator::Plate );
 sub base_columns {
 # acs - 20_05_13 - redmine 10545 - add cassette resistance
 #    my @allele_cols = ( "Vector", "Design", "Gene Id", "Gene Symbol", "Cassette", "Recombinases" );
-    my @allele_cols = ( "Vector", "Design", "Gene Id", "Gene Symbol", "Cassette", "Cassette Resistance", "Recombinases" );
+    my @allele_cols = ( "Vector", "Design", "Gene Id", "Gene Symbol", "Cassette", "Cassette Resistance", "Vector Recombinases", "Cell Recombinases" );
     return ( "Well Name", "Created By", "Created At", "Assay Pending", "Assay Complete", "Accepted?",
         map( { "First Allele $_" } @allele_cols ),
 		map( { "Second Alelle $_" } @allele_cols ),
@@ -38,13 +38,15 @@ sub base_data {
         $first_allele->cassette->name,
         # acs - 20_05_13 - redmine 10545 - add cassette resistance
         $first_allele->cassette->resistance,
-        join( q{/}, @{ $first_allele->recombinases_with_labels() } ),
+        join( q{/}, @{ $first_allele->vector_recombinases } ),
+        join( q{/}, @{ $first_allele->cell_recombinases } ),
         $second_allele->final_vector->as_string,
         $self->design_and_gene_cols( $second_allele ),
         $second_allele->cassette->name,
         # acs - 20_05_13 - redmine 10545 - add cassette resistance
         $second_allele->cassette->resistance,
-        join( q{/}, @{ $second_allele->recombinases_with_labels() } ),
+        join( q{/}, @{ $second_allele->vector_recombinases } ),
+        join( q{/}, @{ $second_allele->cell_recombinases } ),
         ( $second_allele->cassette->promoter ? 'promoter' : 'promoterless' ),
     );
 }
