22bc0279dc6beefc45bf408ccf9a58b65cd247ff
Tiago Grego <tg6@sanger.ac.uk>
2013-09-18 15:36:20 +0100

    non functional DesignTargets grid

--- root/static/appjs/DesignTargets/store/DesignTargets.js
+++
@@ -1,5 +0,0 @@
-Ext.define('DesignTargetsApp.store.DesignTargets', {
-    extend: 'Ext.data.Store',
-    requires: 'DesignTargetsApp.model.DesignTarget',
-    model: 'DesignTargetsApp.model.DesignTarget'
-});
