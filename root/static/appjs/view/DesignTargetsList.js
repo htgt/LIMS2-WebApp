16e4eb0c324ea997e341da1148e065f7be9950ef
Tiago Grego <tg6@sanger.ac.uk>
2013-09-19 13:51:32 +0100

    connecting json

---
+++ root/static/appjs/view/DesignTargetsList.js
@@ -0,0 +1,7 @@
+Ext.define('DesignTargetsApp.view.DesignTargetsList', {
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.DesignTargetsDiv',
+
+    title: 'title here',
+    html: 'html here'
+});
