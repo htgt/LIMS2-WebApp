562d12743d808cee44c90278b52e7504e8d40bd1
Sajith Perera <sajith.perera@gmail.com>
2013-10-02 09:11:03 +0100

    move methods on model that generate qc well eng seq params into the EngSeqParams util module

--- lib/LIMS2/Model/Plugin/Well.pm
+++ lib/LIMS2/Model/Plugin/Well.pm
@@ -9,12 +9,6 @@ use List::MoreUtils qw (any uniq);
 use LIMS2::Model::Util::ComputeAcceptedStatus qw( compute_accepted_status );
 use namespace::autoclean;
 use LIMS2::Model::ProcessGraph;
-use LIMS2::Model::Util::EngSeqParams qw(
-    fetch_design_eng_seq_params
-    fetch_well_eng_seq_params
-    add_display_id
-    fetch_crispr_vector_eng_seq_params
-);
 use LIMS2::Model::Util::RankQCResults qw( rank );
 use LIMS2::Model::Util::DataUpload qw( parse_csv_file );
 use LIMS2::Model::Util::CreateProcess qw( create_process_aux_data_recombinase );
@@ -153,7 +147,6 @@ sub delete_well_accepted_override {
 
     return;
 }
-
 
 sub pspec_create_well_accepted_override {
     return {
@@ -741,70 +734,6 @@ sub get_well_colony_pick_fields_values {
         }
     }
     return \%fields;
-}
-
-sub pspec_generate_eng_seq_params {
-	return {
-        plate_name  => { validate => 'existing_plate_name', optional => 1 },
-        well_name   => { validate => 'well_name', optional => 1 },
-        well_id     => { validate => 'integer', rename => 'id', optional => 1 },
-        cassette    => { validate => 'existing_cassette', optional => 1 },
-        backbone    => { validate => 'existing_backbone', optional => 1 },
-        recombinase => { validate => 'existing_recombinase', default => [], optional => 1 },
-	}
-}
-
-#TODO maybe move bulk of this into Util::EngSeqParams?
-sub generate_well_eng_seq_params{
-    my ( $self, $params ) = @_;
-
-	my $validated_params = $self->check_params( $params, $self->pspec_generate_eng_seq_params );
-
-    my $well = $self->retrieve_well( { slice_def $validated_params, qw( plate_name well_name id ) } );
-    $self->throw( NotFound => { entity_class => 'Well', search_params => $params })
-        unless $well;
-
-    my $design = $well->design;
-    unless ( $design ) {
-        my $crispr = $well->crispr;
-        unless ($crispr) {
-            $self->throw( Implementation => 'Can not produce eng seq params for well that has'
-                                          . ' neither a design or crispr ancestor' )
-        }
-
-        return $self->_generate_crispr_eng_seq_params( $well, $crispr, $validated_params );
-    }
-
-    my $design_data = $design->as_hash;
-
-    # Infer stage from plate type information
-    my $plate_type_descr = $well->plate->type->description;
-    my $stage = $plate_type_descr =~ /ES/ ? 'allele' : 'vector';
-
-    my $design_params = fetch_design_eng_seq_params($design_data);
-
-    my $input_params = {slice_def $validated_params, qw( cassette backbone recombinase targeted_trap)};
-    $input_params->{is_allele} = 1 if $stage eq 'allele';
-    $input_params->{design_type} = $design_data->{type};
-    $input_params->{design_cassette_first} = $design_data->{cassette_first};
-
-    my ($method,$well_params) = fetch_well_eng_seq_params($well, $input_params );
-
-    my $eng_seq_params = { %$design_params, %$well_params };
-    add_display_id($stage, $eng_seq_params);
-
-    return $method, $well->id, $eng_seq_params;
-}
-
-sub _generate_crispr_eng_seq_params {
-    my ( $self, $well, $crispr, $validated_params ) = @_;
-    my $params = {slice_def $validated_params, qw( cassette backbone )};
-    my $backbone = $params->{backbone};
-    $self->throw( Validation => 'Can not specify a cassette for crispr well' ) if $params->{cassette};
-
-    my ($method,$eng_seq_params) = fetch_crispr_vector_eng_seq_params( $crispr, $well, $backbone );
-
-    return $method, $well->id, $eng_seq_params;
 }
 
 sub pspec_retrieve_well_phase_matched_cassette {
