43d92d29359da92e593cfcc166e5f305eb6fa1be
Tiago Grego <tg6@sanger.ac.uk>
2013-10-22 15:49:20 +0100

    Added Electroporation Print report

---
+++ lib/LIMS2/Report/EPPrint.pm
@@ -0,0 +1,88 @@
+package LIMS2::Report::EPPrint;
+
+use Moose;
+use List::MoreUtils qw( apply );
+use namespace::autoclean;
+use Log::Log4perl qw( :easy );
+
+extends qw( LIMS2::ReportGenerator::Plate::SimpleColumns );
+
+override plate_types => sub {
+    return [ 'EP' ];
+};
+
+override _build_name => sub {
+    my $self = shift;
+
+    return 'EP Plate ' . $self->plate_name;
+};
+
+override _build_columns => sub {
+    my $self = shift;
+
+    my @columns = (
+        'Observed ID',
+        'Symbol',
+        'DNA Plate',
+        'DNA Well',
+        'EP Well',
+    );
+
+    return \@columns;
+};
+
+override iterator => sub {
+    my $self = shift;
+
+    my $plate = $self->plate_name;
+    my %well_hash;
+    my @well_list = ('A01','B01','C01','D01','E01','A02','B02','C02','D02','E02','A03','B03',
+            'C03','D03','E03','A04','B04','C04','D04','E04','A05','B05','C05','D05','E05',);
+
+    foreach my $well (@well_list) {
+        $well_hash{$well}{ob_id} = '';
+        $well_hash{$well}{symbol} = '';
+        $well_hash{$well}{dna_plate} = '';
+        $well_hash{$well}{dna_well} = '';
+        $well_hash{$well}{ep_well} = "$plate".'_'."$well";
+    }
+
+    my $wells_rs = $self->plate->search_related(
+        wells => {},
+        {
+            prefetch => ['well_accepted_override',
+                         'well_colony_counts']
+        }
+    );
+
+    while (my $well = $wells_rs->next) {
+
+        $well_hash{$well->name}{symbol} = $self->design_and_gene_cols( $well );
+        my $parent_well = $well->get_input_wells_as_string;
+        $parent_well =~ m/(.*)\[(.*)\]/xgms;
+        $well_hash{$well->name}{dna_plate} = $1;
+        $well_hash{$well->name}{dna_well} = $2;
+
+    }
+
+    my @epprint;
+    foreach my $well (@well_list) {
+        push (@epprint, [
+            $well_hash{$well}{ob_id},
+            $well_hash{$well}{symbol},
+            $well_hash{$well}{dna_plate},
+            $well_hash{$well}{dna_well},
+            $well_hash{$well}{ep_well},
+            ]);
+    }
+
+    return Iterator::Simple::iter (\@epprint);
+
+};
+
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
