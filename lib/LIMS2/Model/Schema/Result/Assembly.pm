8cf724776c43c91b964f8460b1b084e5ac24e885
Alex Hodgkins <ah19@sanger.ac.uk>
2013-11-01 14:12:45 +0000

    Ran schema dumper to get unique constraint from crispr pairs table, renamed CrisprOffTargetSummaries to CrisprOffTargetSummary

--- lib/LIMS2/Model/Schema/Result/Assembly.pm
+++ lib/LIMS2/Model/Schema/Result/Assembly.pm
@@ -178,8 +178,8 @@ __PACKAGE__->has_many(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07022 @ 2013-06-04 13:23:46
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:yQU2EjXE+KefvUlPfOH1TQ
+# Created by DBIx::Class::Schema::Loader v0.07022 @ 2013-11-01 12:02:54
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:bWlElt9mequLm4Q8zk40Kg
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
