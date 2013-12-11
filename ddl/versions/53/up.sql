e937ca5e3cd378dcfbcc95032606ac33aae1dc77
Sajith Perera <sajith.perera@gmail.com>
2013-12-11 08:41:09 +0000

    rename table to design_attempts from design_create, add species_id column

--- ddl/versions/53/up.sql
+++ ddl/versions/53/up.sql
@@ -1,12 +1,13 @@
-CREATE TABLE design_create (
+CREATE TABLE design_attempts (
     id                SERIAL PRIMARY KEY,
     design_parameters TEXT,
     gene_id           TEXT,
     status            TEXT,
     fail              TEXT,
     error             TEXT,
-    design_ids        TEXT, 
+    design_ids        TEXT,
+    species_id        TEXT NOT NULL REFERENCES species(id),
     created_by        INTEGER NOT NULL REFERENCES users(id),
     created_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    comment           TEXT 
+    comment           TEXT
 );
