b2e0c008fc44047b7231a972d8e8cf95752f35d0
David Parry-Smith <david.parry-smith@sanger.ac.uk>
2014-07-15 12:16:04 +0100

    Corrected schema files for crispr and genotyping primers

--- lib/LIMS2/Model/Schema/Result/GenotypingPrimersLoci.pm
+++ lib/LIMS2/Model/Schema/Result/GenotypingPrimersLoci.pm
@@ -139,5 +139,21 @@ __PACKAGE__->belongs_to(
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+=head2 genotyping_primer
+
+Type: belongs_to
+
+Related object: L<LIMS2::Model::Schema::Result::GenotypingPrimer>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "genotyping_primer",
+  "LIMS2::Model::Schema::Result::GenotypingPrimer",
+  { id => "genotyping_primer_id" },
+  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+);
+
 __PACKAGE__->meta->make_immutable;
 1;
