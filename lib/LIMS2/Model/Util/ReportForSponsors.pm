fdd2753708768a791372b07cd8a50aa3acb7358a
Tiago Grego <tg6@sanger.ac.uk>
2014-07-17 09:39:00 +0100

    changed sort order of the sponsor summary report from gene name to progress

--- lib/LIMS2/Model/Util/ReportForSponsors.pm
+++ lib/LIMS2/Model/Util/ReportForSponsors.pm
@@ -879,8 +879,16 @@ SQL_END
         DEBUG "crispr counts done";
     }
 
-    # sort the array by gene symbol
-    my @sorted_genes_for_display =  sort { $a->{ 'gene_symbol' } cmp $b-> { 'gene_symbol' } } @genes_for_display;
+    my @sorted_genes_for_display =  sort {
+            $b->{ 'targeted_clones' }       <=> $a->{ 'targeted_clones' }       ||
+            $b->{ 'colonies_picked' }       <=> $a->{ 'colonies_picked' }       ||
+            $b->{ 'electroporations' }      <=> $a->{ 'electroporations' }      ||
+            $b->{ 'passing_vector_wells' }  <=> $a->{ 'passing_vector_wells' }  ||
+            $b->{ 'accepted_vector_wells' } <=> $a->{ 'accepted_vector_wells' } ||
+            $b->{ 'vector_wells' }          <=> $a->{ 'vector_wells' }          ||
+            $b->{ 'vector_designs' }        <=> $a->{ 'vector_designs' }        ||
+            $a->{ 'gene_symbol' }           cmp $b->{ 'gene_symbol' }
+        } @genes_for_display;
 
     return \@sorted_genes_for_display;
 }
