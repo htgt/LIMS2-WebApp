f80ef1088e997128f36f1ba342df1e7fd296b441
Sajith Perera <sajith.perera@gmail.com>
2014-09-24 14:16:51 +0100

    add as_hash methods to genotyping primer resultsets, and crispr primers loci resultset

--- lib/LIMS2/Model/Schema/Result/GenotypingPrimer.pm
+++ lib/LIMS2/Model/Schema/Result/GenotypingPrimer.pm
@@ -165,10 +165,18 @@ __PACKAGE__->has_many(
 sub as_hash {
     my $self = shift;
 
+    my $locus;
+    if ( my $default_assembly = $self->design->species->default_assembly ) {
+        $locus = $self->search_related( 'genotyping_primer_loci',
+            { assembly_id => $default_assembly->assembly_id } )->first;
+    }
+
     return {
-        id   => $self->id,
-        type => $self->genotyping_primer_type_id,
-        seq  => $self->seq
+        id      => $self->id,
+        type    => $self->genotyping_primer_type_id,
+        seq     => $self->seq,
+        species => $self->design->species_id,
+        locus   => $locus ? $locus->as_hash : undef,
     };
 }
 
