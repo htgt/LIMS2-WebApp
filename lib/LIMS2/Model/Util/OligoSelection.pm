5160790bccd86e34d99b4f51900fa948b27715ca
David Parry-Smith <david.parry-smith@sanger.ac.uk>
2014-10-01 17:03:26 +0100

    Reorganised files for genoverse views and genoverse support
    Genoverse views were originally in the crisprs section but all genoverse
    support code is now grouped more coherently across the MVC.

--- lib/LIMS2/Model/Util/OligoSelection.pm
+++ lib/LIMS2/Model/Util/OligoSelection.pm
@@ -21,10 +21,8 @@ use Sub::Exporter -setup => {
         pick_single_crispr_primers
         pick_crispr_PCR_primers
         retrieve_crispr_primers
-        get_genotyping_primer_extent
-        get_design_extent
-        get_gene_extent
         retrieve_crispr_data_for_id
+        get_db_genotyping_primers_as_hash
     ) ]
 };
 
@@ -1270,121 +1268,6 @@ sub get_db_genotyping_primers_as_hash {
     }
 
     return \%g_primer_hash;
-}
-
-
-=head get_genotyping_extent
-
-Given
-
-Returns
-hashref:
-    start_coord
-    end_coord
-    chr_name
-    assembly
-=cut
-
-sub get_genotyping_primer_extent {
-    my $schema = shift;
-    my $params = shift;
-    my $species = shift;
-
-    my $g_primer_hash = get_db_genotyping_primers_as_hash($schema, $params );
-
-    if ( ! %{$g_primer_hash} ) {
-        LIMS2::Exception->throw( 'No data returned for design primers' );
-    }
-
-    my %extent_hash;
-
-    # Simply compare all the start and end positions (chr_start, chr_end) and take the min of chr_start and the max of chr_end
-
-    $extent_hash{'chr_start'} = $g_primer_hash->{'GF1'}->{'chr_start'};
-    $extent_hash{'chr_end'} = $g_primer_hash->{'GF1'}->{'chr_end'};
-    while ( my ($primer, $vals) = each %$g_primer_hash ) {
-        $extent_hash{'chr_start'} = $vals->{'chr_start'} if $extent_hash{'chr_start'} > $vals->{'chr_start'};
-        $extent_hash{'chr_end'} = $vals->{'chr_end'} if $extent_hash{'chr_end'} < $vals->{'chr_start'};
-    }
-    $extent_hash{'chr_name'} = $g_primer_hash->{'GF1'}->{'chr_name'};
-    $extent_hash{'assembly'} = get_species_default_assembly( $schema, $species);
-
-    return \%extent_hash;
-}
-
-sub get_gene_extent {
-    my $model = shift;
-    my $params = shift;
-    my $species = shift;
-
-    my $ensembl_stable_id;
-
-    $ensembl_stable_id = $model->find_gene({
-            species => $species,
-            search_term => $params->{'gene_id'}
-        })->{'ensembl_id'};
-
-    DEBUG ( $params->{'gene_id'} . ' = ' . $ensembl_stable_id );
-
-    my %extent_hash;
-
-    my $slice_adaptor = $model->ensembl_slice_adaptor($species, 'Core', 'Slice');
-    my $slice = $slice_adaptor->fetch_by_gene_stable_id( $ensembl_stable_id, 5e3 );
-
-    my $coord_sys  = $slice->coord_system()->name();
-    my $seq_region = $slice->seq_region_name();
-    my $start      = $slice->start();
-    my $end        = $slice->end();
-    my $strand     = $slice->strand();
-
-    DEBUG ("Slice: $coord_sys $seq_region $start-$end ($strand)");
-
-    $extent_hash{'chr_start'} = $start;
-    $extent_hash{'chr_end'} = $end;
-    $extent_hash{'chr_name'} = $seq_region;
-    $extent_hash{'assembly'} = get_species_default_assembly( $model->schema, $species);
-
-    return \%extent_hash;
-}
-
-sub get_design_extent {
-    my $model = shift;
-    my $params = shift;
-    my $species = shift;
-
-    my $design_r = $model->schema->resultset('Design')->find($params->{'design_id'});
-
-    my $design_info = LIMS2::Model::Util::DesignInfo->new( design => $design_r );
-    my $design_oligos = $design_info->oligos;
-
-    my %extent_hash;
-
-    # Simply compare all the start and end positions (chr_start, chr_end) and take the min of chr_start and the max of chr_end
-    my @label_keys = keys %$design_oligos;
-    my $arbitrary_key = $label_keys[0];
-
-    $extent_hash{'chr_start'} = $design_oligos->{$arbitrary_key}->{'start'};
-    $extent_hash{'chr_end'} = $design_oligos->{$arbitrary_key}->{'end'};
-    while ( my ($primer, $vals) = each %$design_oligos ) {
-        $extent_hash{'chr_start'} = $vals->{'start'} if $extent_hash{'chr_start'} > $vals->{'start'};
-        $extent_hash{'chr_end'} = $vals->{'end'} if $extent_hash{'chr_end'} < $vals->{'start'};
-    }
-    $extent_hash{'chr_name'} = $design_oligos->{$arbitrary_key}->{'chromosome'};
-    $extent_hash{'assembly'} = get_species_default_assembly( $model->schema, $species);
-
-    return \%extent_hash;
-}
-
-
-
-sub get_species_default_assembly {
-    my $schema = shift;
-    my $species = shift;
-
-    my $assembly_r = $schema->resultset('SpeciesDefaultAssembly')->find( { species_id => $species } );
-
-    return $assembly_r->assembly_id || undef;
-
 }
 
 sub retrieve_crispr_data_for_id {
--- lib/LIMS2/WebApp/Controller/API/Browser.pm
+++ lib/LIMS2/WebApp/Controller/API/Browser.pm
@@ -40,11 +40,12 @@ sub crispr :Path('/api/crispr') :Args(0) :ActionClass('REST') {
 sub crispr_GET {
     my ( $self, $c ) = @_;
 
-    my $schema = $c->model('Golgi')->schema;
+    my $model = $c->model('Golgi');
+    my $schema = $model->schema;
 
     my $params = ();
     $params->{species} = $c->session->{'selected_species'} // 'Human';
-    $params->{assembly_id} = $c->request->params->{'assembly'} // get_species_default_assembly($schema, $params->{species} ) // 'GRCh37';
+    $params->{assembly_id} = $c->request->params->{'assembly'} // $model->get_species_default_assembly( $params->{species} ) // 'GRCh37';
     $params->{chromosome_number}= $c->request->params->{'chr'};
     $params->{start_coord}= $c->request->params->{'start'};
     $params->{end_coord}= $c->request->params->{'end'};
@@ -66,11 +67,12 @@ sub crispr_pairs :Path('/api/crispr_pairs') :Args(0) :ActionClass('REST') {
 sub crispr_pairs_GET {
     my ( $self, $c ) = @_;
 
-    my $schema = $c->model('Golgi')->schema;
+    my $model = $c->model('Golgi');
+    my $schema = $model->schema;
 
     my $params = ();
     $params->{species} = $c->session->{'selected_species'} // 'Human';
-    $params->{assembly_id} = $c->request->params->{'assembly'} // get_species_default_assembly($schema, $params->{species} ) // 'GRCh37';
+    $params->{assembly_id} = $c->request->params->{'assembly'} // $model->get_species_default_assembly( $params->{species} ) // 'GRCh37';
     $params->{chromosome_number}= $c->request->params->{'chr'};
     $params->{start_coord}= $c->request->params->{'start'};
     $params->{end_coord}= $c->request->params->{'end'};
@@ -93,11 +95,12 @@ sub crispr_groups :Path('/api/crispr_groups') :Args(0) :ActionClass('REST') {
 sub crispr_groups_GET {
     my ( $self, $c ) = @_;
 
-    my $schema = $c->model('Golgi')->schema;
+    my $model = $c->model('Golgi');
+    my $schema = $model->schema;
 
     my $params = ();
     $params->{species} = $c->session->{'selected_species'} // 'Human';
-    $params->{assembly_id} = $c->request->params->{'assembly'} // get_species_default_assembly($schema, $params->{species} ) // 'GRCh37';
+    $params->{assembly_id} = $c->request->params->{'assembly'} // $model->get_species_default_assembly( $params->{species} ) // 'GRCh37';
     $params->{chromosome_number}= $c->request->params->{'chr'};
     $params->{start_coord}= $c->request->params->{'start'};
     $params->{end_coord}= $c->request->params->{'end'};
@@ -115,27 +118,18 @@ sub crispr_groups_GET {
 }
 
 
-sub get_species_default_assembly {
-    my $schema = shift;
-    my $species = shift;
-
-    my $assembly_r = $schema->resultset('SpeciesDefaultAssembly')->find( { species_id => $species } );
-
-    return $assembly_r->assembly_id || undef;
-
-}
-
 sub gibson_designs :Path('/api/gibson_designs') :Args(0) :ActionClass('REST') {
 }
 
 sub gibson_designs_GET {
     my ( $self, $c ) = @_;
 
-    my $schema = $c->model('Golgi')->schema;
+    my $model = $c->model('Golgi');
+    my $schema = $model->schema;
 
     my $params = ();
     $params->{species} = $c->session->{'selected_species'} // 'Human';
-    $params->{assembly_id} = $c->request->params->{'assembly'} // get_species_default_assembly($schema, $params->{species} ) // 'GRCh37';
+    $params->{assembly_id} = $c->request->params->{'assembly'} // $model->get_species_default_assembly( $params->{species} ) // 'GRCh37';
     $params->{chromosome_number}= $c->request->params->{'chr'};
     $params->{start_coord}= $c->request->params->{'start'};
     $params->{end_coord}= $c->request->params->{'end'};
@@ -157,11 +151,12 @@ sub generic_designs :Path('/api/generic_designs') :Args(0) :ActionClass('REST') 
 sub generic_designs_GET {
     my ( $self, $c ) = @_;
 
-    my $schema = $c->model('Golgi')->schema;
+    my $model = $c->model('Golgi');
+    my $schema = $model->schema;
 
     my $params = ();
     $params->{species} = $c->session->{'selected_species'} // 'Human';
-    $params->{assembly_id} = $c->request->params->{'assembly'} // get_species_default_assembly($schema, $params->{species} ) // 'GRCh37';
+    $params->{assembly_id} = $c->request->params->{'assembly'} // $model->get_species_default_assembly( $params->{species} ) // 'GRCh37';
     $params->{chromosome_number}= $c->request->params->{'chr'};
     $params->{start_coord}= $c->request->params->{'start'};
     $params->{end_coord}= $c->request->params->{'end'};
