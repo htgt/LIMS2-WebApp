acc38dbb26602cdb46b8ac3596654fda03b5e3cc
Sajith Perera <sajith.perera@gmail.com>
2014-10-08 09:59:39 +0100

    moved the well_genotyping_info page to the public_reports section so no authentication is required to view these pages, also replaced nav bar with just public report links if there is no user set

--- lib/LIMS2/WebApp/Controller/PublicReports.pm
+++ lib/LIMS2/WebApp/Controller/PublicReports.pm
@@ -1,6 +1,7 @@
 package LIMS2::WebApp::Controller::PublicReports;
 use Moose;
 use LIMS2::Report;
+use Try::Tiny;
 use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller'; }
@@ -63,6 +64,68 @@ sub download_report :Path( '/public_reports/download' ) :Args(1) {
     $c->response->content_type( 'text/csv' );
     $c->response->header( 'Content-Disposition' => "attachment; filename=$report_name.csv" );
     $c->response->body( $report_fh );
+    return;
+}
+
+=head2 well_genotyping_info
+
+Page to choose the desired well, no arguments
+
+=cut
+sub well_genotyping_info_search :Path( '/public_reports/well_genotyping_info_search' ) :Args(0) {
+    my ( $self, $c ) = @_;
+
+    return;
+}
+
+=head2 well_genotyping_info
+
+Page to display chosen well, takes a well id (later a barcode) or a plate/well combo
+
+=cut
+sub well_genotyping_info :Path( '/public_reports/well_genotyping_info' ) :Args() {
+    my ( $self, $c, @args ) = @_;
+
+    if ( @args == 1 ) {
+        my $barcode = shift @args;
+
+        $self->_stash_well_genotyping_info( $c, { barcode => $barcode } );
+    }
+    elsif ( @args == 2 ) {
+        my ( $plate_name, $well_name ) = @args;
+
+        $self->_stash_well_genotyping_info(
+            $c, { plate_name => $plate_name, well_name => $well_name }
+        );
+    }
+    else {
+        $c->stash( error_msg => "Invalid number of arguments" );
+    }
+
+    return;
+}
+
+sub _stash_well_genotyping_info {
+    my ( $self, $c, $search ) = @_;
+
+    #well_id will become barcode
+    my $well = $c->model('Golgi')->retrieve_well( $search );
+
+    unless ( $well ) {
+        $c->stash( error_msg => "Well doesn't exist" );
+        return;
+    }
+
+    try {
+        #needs to be given a method for finding genes
+        my $data = $well->genotyping_info( sub { $c->model('Golgi')->find_genes( @_ ); } );
+        $c->stash( data => $data );
+    }
+    catch {
+        #get string representation if its a lims2::exception
+        $c->stash( error_msg => ref $_ && $_->can('as_string') ? $_->as_string : $_ );
+    };
+
     return;
 }
 
