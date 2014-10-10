74284455931728d82a6a41e3af098c224af130ad
Tiago Grego <tg6@sanger.ac.uk>
2014-10-10 11:46:05 +0100

    added rest api for effort priority update

--- lib/LIMS2/WebApp/Controller/API/Project.pm
+++ lib/LIMS2/WebApp/Controller/API/Project.pm
@@ -76,5 +76,23 @@ sub project_recovery_comment_GET{
 }
 
 
+sub project_priority : Path( '/api/project_priority' ) : Args(0) : ActionClass( 'REST' ) {
+}
+
+sub project_priority_GET{
+    my ( $self, $c ) = @_;
+
+    $c->assert_user_roles('read');
+
+    my $project = $c->model( 'Golgi' )->txn_do(
+        sub {
+            shift->retrieve_project_by_id( { id => $c->request->param( 'id' ) } );
+        }
+    );
+    $project->update( { priority => $c->request->param( 'priority' ) } );
+
+    return $self->status_ok( $c, entity => $project->as_hash );
+}
+
 
 1;
