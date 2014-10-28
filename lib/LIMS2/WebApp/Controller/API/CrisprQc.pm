f003b56e9e525aa747f1f1264e75373c07a693c8
Sajith Perera <sajith.perera@gmail.com>
2014-10-28 09:43:58 +0000

    add new crispr es qc api controller, transfer update_well_accepted method, add method to update the  crispr damage value of a record

---
+++ lib/LIMS2/WebApp/Controller/API/CrisprQc.pm
@@ -0,0 +1,85 @@
+package LIMS2::WebApp::Controller::API::CrisprQc;
+use Moose;
+use namespace::autoclean;
+use Try::Tiny;
+
+BEGIN {extends 'LIMS2::Catalyst::Controller::REST'; }
+
+=head1 NAME
+
+LIMS2::WebApp::Controller::API::CrisprQc - Catalyst Controller
+
+=head1 DESCRIPTION
+
+API methods dealing with es cell crispr qc
+
+=cut
+
+sub update_well_accepted :Path('/api/update_well_accepted') :Args(0) :ActionClass('REST') {
+}
+
+sub update_well_accepted_POST {
+    my ( $self, $c ) = @_;
+
+    my $params = $c->request->params;
+
+    my $qc_well = $c->model('Golgi')->schema->resultset('CrisprEsQcWell')->find(
+        {
+            well_id             => $params->{well_id},
+            crispr_es_qc_run_id => $params->{qc_run_id},
+        },
+        { prefetch => 'well' }
+    );
+
+    #TODO: validate params
+
+    #set both the qc well and the actual well to accepted
+    try {
+        $c->model('Golgi')->txn_do(
+            sub {
+                $qc_well->update( { accepted => $params->{accepted} } );
+                $qc_well->well->update( { accepted => $params->{accepted} } );
+            }
+        );
+        $self->status_ok( $c, entity => { success => 1 } );
+    }
+    catch {
+        $self->status_bad_request( $c, message => "Error: $_" );
+        $c->log->error( "Error updating crispr es qc well accepted flag: $_" );
+    };
+
+    return;
+}
+
+sub update_well_crispr_damage :Path('/api/update_well_crispr_damage') :Args(0) :ActionClass('REST') {
+}
+
+sub update_well_crispr_damage_POST {
+    my ( $self, $c ) = @_;
+
+    try{
+        my $qc_well = $c->model('Golgi')->txn_do(
+            sub {
+                shift->update_crispr_well_damage( $c->request->params );
+            }
+        );
+        $self->status_ok( $c, entity => { success => 1 } );
+    }
+    catch {
+        $c->log->error( "Error updating crispr well damage value: $_" );
+        $self->status_bad_request( $c, message => "Error: $_" );
+    };
+
+    return
+}
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
