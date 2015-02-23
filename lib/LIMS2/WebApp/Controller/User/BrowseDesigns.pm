08dfdb017c6ae05d36667f910a95075e6491c092
Sajith Perera <sajith.perera@gmail.com>
2015-02-23 07:47:23 +0000

    update design view page to show nonsense crispr details

--- lib/LIMS2/WebApp/Controller/User/BrowseDesigns.pm
+++ lib/LIMS2/WebApp/Controller/User/BrowseDesigns.pm
@@ -81,7 +81,11 @@ sub view_design : Path( '/user/view_design' ) : Args(0) {
 
     my $ucsc_db = $UCSC_BLAT_DB{ lc( $species_id) };
 
-    my ( $crisprs, $crispr_pairs, $crispr_groups ) = crisprs_for_design( $c->model('Golgi'), $design );
+    my ( $crisprs, $crispr_pairs, $crispr_groups ) = ( [], [], [] );
+    # Only want to show the one linked crispr for nonsense designs
+    if ( $design_data->{type} ne 'nonsense' ) {
+        ( $crisprs, $crispr_pairs, $crispr_groups ) = crisprs_for_design( $c->model('Golgi'), $design );
+    }
     my $design_attempt = $design->design_attempt;
 
     my $group_ids = join ", ", map { $_->id } @$crispr_groups;
