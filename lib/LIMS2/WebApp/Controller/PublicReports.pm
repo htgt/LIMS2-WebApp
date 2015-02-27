3f5672ede386b57accf42909e7f542c18c9292a0
Tiago Grego <tg6@sanger.ac.uk>
2015-02-27 11:28:44 +0000

    gene name search on public_gene_report changed to find_gene

--- lib/LIMS2/WebApp/Controller/PublicReports.pm
+++ lib/LIMS2/WebApp/Controller/PublicReports.pm
@@ -551,7 +551,10 @@ sub public_gene_report :Path( '/public_reports/gene_report' ) :Args(1) {
     my $gene_symbol;
     my %targeted_clones;
     while ( my $sr = $design_summaries_rs->next ) {
-        $gene_symbol = $sr->design_gene_symbol unless $gene_symbol;
+        $gene_symbol = $model->find_gene({
+                    species => $species,
+                    search_term => $gene_id
+                })->{'gene_symbol'};
         next if exists $targeted_clones{ $sr->ep_pick_well_id };
 
         my %data;
