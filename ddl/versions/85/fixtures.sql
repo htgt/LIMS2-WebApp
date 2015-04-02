61fa678d87a18d2962b86e6a39590a0d45637e3d
Sajith Perera <sajith.perera@gmail.com>
2015-04-02 10:09:02 +0100

    add new tables to enable tracking for crispr_tracker_rna as part of a process

---
+++ ddl/versions/85/fixtures.sql
@@ -0,0 +1,5 @@
+INSERT INTO schema_versions(version) VALUES (85);
+
+INSERT INTO process_types (id,description) VALUES ('oligo_assembly', 'Oligo and crispr assembly');
+
+INSERT INTO plate_types (id,description) VALUES ('OLIGO_ASSEMBLY', 'Oligo and crispr assembly');
