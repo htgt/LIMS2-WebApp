0ce4c8f3b18097f9a206194521c858949d58d02e
af11_sanger <af11@sanger.ac.uk>
2015-06-22 14:25:14 +0100

    attempt to improve performance

--- lib/LIMS2/Report/CrisprEPSummaryJune2015.pm
+++ lib/LIMS2/Report/CrisprEPSummaryJune2015.pm
@@ -6,10 +6,11 @@ use strict;
 use Moose;
 use Log::Log4perl qw(:easy);
 use namespace::autoclean;
-use List::MoreUtils qw( uniq );
+use List::MoreUtils qw( uniq any );
 use Try::Tiny;
 use Data::Dumper;
 use Math::Round;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 extends qw( LIMS2::ReportGenerator );
 
@@ -108,24 +109,11 @@ sub _build_column_map {
         { 'no-call'                => "No Calls" },
         { targeting_efficiency     => "Biallelic Targeting Efficiency" },
         { recovery_comment         => "Recovery Comment" },
-        #{ ep_colonies_picked       => "EP Colonies Picked" },
-        #{ ep_colonies_total        => "EP Colonies Total" },
-        #{ ep_pick_qc_seq_pass      => "EP Pick Qc Seq Pass" },
-        #{ ep_pick_well_accepted    => "EP Pick Well Accepted" },
     ];
 }
 
 override _build_columns => sub {
     my $self = shift;
-
-    #there are now non sumamry fields so we cant do this
-    # my @fields;
-    # for my $field ( @{ $self->summary_fields } ) {
-    #     #capitalise every letter and replace _ with a space
-    #     push @fields, join " ", map { ucfirst } split /_/, $field;
-    # }
-
-    # return \@fields;
 
     return [ map { values %{$_} } @{ $self->column_map } ];
 };
@@ -151,6 +139,25 @@ sub fmt_bool {
     return $data;
 }
 
+# Get resultset and store each record in a hash where result->id is the key
+sub resultset_by_id{
+    my ($self,$resultset,$search,$atts) = @_;
+
+    my $t0 = [gettimeofday];
+
+    my $rs = $self->model->schema->resultset($resultset)->search($search,$atts);
+
+    DEBUG "Time take for $resultset query: ".tv_interval ( $t0 );
+
+    my %results_by_id;
+    while (my $result = $rs->next){
+        $results_by_id{$result->id} = $result;
+    }
+    DEBUG "Time taken for $resultset query and store: ".tv_interval($t0);
+
+    return \%results_by_id;
+}
+
 sub _well_name {
     my $well = shift;
 
@@ -162,139 +169,144 @@ sub build_ep_detail {
 
     my @row;
 
-    # get all crispr_ep plates
-    my @crispr_ep_plates = $self->model->schema->resultset('Plate')->search({
-        type_id => 'CRISPR_EP',
-        species_id => $self->species,
-        #sponsor_id => 'EUCOMMTools Recovery',
-        #name => { -in => ['HUEP0001','HUEP0002','HUEP0003','HUEP0004','HUEP0005',] } ## FIXME: single plate for testing
+    my $species = $self->model->schema->resultset('Species')->find({ id => $self->species});
+    my $assembly_id = $species->default_assembly->assembly_id;
+
+    my @summary = $self->model->schema->resultset('Summary')->search({
+            crispr_ep_plate_name => { '!=', undef },
+            design_species_id    => $self->species,
+        },
+        {
+            select   => $self->summary_fields,
+            group_by => $self->summary_fields,
+            order_by => ['crispr_ep_plate_name','crispr_ep_well_name'],
     });
 
-    DEBUG "Creating summary for ".scalar(@crispr_ep_plates)." CRISPR_EP plates";
-    # for every crispr_ep well
+    # Retrieve assembly wells and crispr ep wells in batch with appropriate relations in prefetch
+    my @assembly_well_ids = uniq map { $_->assembly_well_id } @summary;
+    my $search = { 'me.id' => { -in => \@assembly_well_ids } };
+    my $atts = { prefetch => 'well_assembly_qcs' };
+    my $assembly_wells_by_id = $self->resultset_by_id('Well',$search,$atts);
 
-    my $assembly_id = $crispr_ep_plates[0]->species->default_assembly->assembly_id;
+    my @crispr_ep_well_ids = uniq map { $_->crispr_ep_well_id } @summary;
+    my $ep_search = { 'me.id' => { -in => \@crispr_ep_well_ids } };
+    my $ep_atts = { prefetch => 'well_colony_counts' };
+    my $crispr_ep_wells_by_id = $self->resultset_by_id('Well',$ep_search,$ep_atts);
 
-    my $search = {};
-    if($self->has_sponsor){
-        $search->{sponsor_id} = $self->sponsor;
-    }
+    for my $summary ( @summary ) {
 
-    for my $crispr_ep_plate ( @crispr_ep_plates ) {
-        $search->{crispr_ep_plate_name} = $crispr_ep_plate->name;
-        my @summary = $self->model->schema->resultset('Summary')->search( $search,
+        # Some CRISPR_EP plates come from OLIGO_ASSEMBLY plates
+        # so do not have assembly well in summaries table
+        # We skip these for now
+        next unless $summary->assembly_well_id;
+
+        my %data;
+
+        $data{$_} = $summary->$_ for @{ $self->summary_fields };
+
+        $data{crispr_ep_plate_well_name} = $summary->crispr_ep_plate_name."_".$summary->crispr_ep_well_name;
+        $data{vector_id} = $summary->final_pick_plate_name."_".$summary->final_pick_well_name;
+
+        my $assembly_well = $assembly_wells_by_id->{$summary->assembly_well_id};
+
+        # Get experiments matching assembly well
+        my @experiments = $assembly_well->experiments;
+        $data{experiment_id} = join ", ", map { $_->id } @experiments;
+
+        # Fetch project related info for all matching experiments
+        DEBUG "Fetching projects for experiments ".$data{experiment_id};
+        my @projects = map { $_->project } @experiments;
+        # Skip if not relevant to requested sponsor
+        my @sponsor_ids_unsorted = map { $_->sponsor_ids } @projects;
+        if($self->has_sponsor){
+            next unless any { $_ eq $self->sponsor } @sponsor_ids_unsorted;
+        }
+        my @sponsor_ids = sort { $a cmp $b } uniq @sponsor_ids_unsorted;
+
+        $data{sponsor_id} = join "/", @sponsor_ids;
+        $data{recovery_comment} = join ", ", map { $_->recovery_comment } @projects;
+
+        # Get chromosome number from design
+        my $design = $self->model->schema->resultset('Design')->find({
+            id => $summary->design_id,
+        });
+        my $design_oligo_locus = $design->oligos->first->search_related( 'loci', { assembly_id => $assembly_id } )->first;
+        $data{chromosome} = $design_oligo_locus->chr->name;
+
+        # Crisprs displayed in no particular order e.g. not left then right
+        # FIXME? is this a problem?
+        # FIXME: will not display crispr groups with more than 2 crisprs
+        my @crisprs = $assembly_well->crisprs;
+        $data{crispr_1_id} = $crisprs[0] ? $crisprs[0]->id : undef;
+        $data{crispr_2_id} = $crisprs[1] ? $crisprs[1]->id : undef;
+
+        # Work out assembly well QC verification result (true or false). NB: in future we may be storing
+        # this as the well_accepted flag in which case can get it from summaries
+        $data{assembly_well_seq_verified} = fmt_bool($assembly_well->assembly_well_qc_verified);
+
+        # New summary to get downstream ep pick wells
+        my @epds = $self->model->schema->resultset('Summary')->search(
+            { crispr_ep_well_id => $summary->crispr_ep_well_id },
             {
-                select   => $self->summary_fields,
-                group_by => $self->summary_fields,
-                order_by => 'crispr_ep_well_name',
+                select => [ qw( ep_pick_well_id ep_pick_well_accepted ) ],
+                group_by => [ qw( ep_pick_well_id ep_pick_well_accepted ) ],
             }
         );
 
-        next unless @summary;
-
-        for my $summary ( @summary ) {
-
-            # Some CRISPR_EP plates come from OLIGO_ASSEMBLY plates
-            # so do not have assembly well in summaries table
-            # We skip these for now
-            next unless $summary->assembly_well_id;
-
-            my %data;
-
-            #push @data, [ map { fmt_bool( $summary->$_ ) } @{ $self->summary_fields } ];
-            $data{$_} = $summary->$_ for @{ $self->summary_fields };
-
-            $data{crispr_ep_plate_well_name} = $summary->crispr_ep_plate_name."_".$summary->crispr_ep_well_name;
-            $data{vector_id} = $summary->final_pick_plate_name."_".$summary->final_pick_well_name;
-
-            my $assembly_well = $self->model->schema->resultset('Well')->find( {
-                id => $summary->assembly_well_id,
-            } );
-            my @experiments = $assembly_well->experiments;
-            $data{experiment_id} = join ", ", map { $_->id } @experiments;
-
-            my $design = $self->model->schema->resultset('Design')->find({
-                id => $summary->design_id,
-            });
-            my $design_oligo_locus = $design->oligos->first->search_related( 'loci', { assembly_id => $assembly_id } )->first;
-            $data{chromosome} = $design_oligo_locus->chr->name;
-
-            DEBUG "Fetching projects for experiments ".$data{experiment_id};
-            my @projects = map { $_->project } @experiments;
-            my @sponsor_ids = sort { $a cmp $b } uniq map { $_->sponsor_ids } @projects;
-            $data{sponsor_id} = join "/", @sponsor_ids;
-            $data{recovery_comment} = join ", ", map { $_->recovery_comment } @projects;
-
-            # Crisprs displayed in no particular order e.g. not left then right
-            # FIXME? is this a problem?
-            my @crisprs = $assembly_well->crisprs;
-            $data{crispr_1_id} = $crisprs[0] ? $crisprs[0]->id : undef;
-            $data{crispr_2_id} = $crisprs[1] ? $crisprs[1]->id : undef;
-
-            # Work out assembly well QC verification result (true or false). NB: in future we may be storing
-            # this as the well_accepted flag in which case can get it from summaries
-            $data{assembly_well_seq_verified} = fmt_bool($assembly_well->assembly_well_qc_verified);
-
-            my @epds = $self->model->schema->resultset('Summary')->search(
-                { crispr_ep_well_id => $summary->crispr_ep_well_id },
-                {
-                    select => [ qw( ep_pick_well_id ep_pick_well_accepted ) ],
-                    group_by => [ qw( ep_pick_well_id ep_pick_well_accepted ) ],
-                }
-            );
-
-            my %num_wells;
-            my $num_accepted = 0;
-            for my $row ( @epds ) {
-                next unless $row->ep_pick_well_id;
-                $num_wells{ $row->ep_pick_well_id }++;
-                $num_accepted++ if $row->ep_pick_well_accepted;
-            }
-
-            $data{ep_colonies_picked} = scalar( keys %num_wells );
-            $data{ep_colonies_accepted} = $num_accepted;
-
-            DEBUG "Finding colonly counts for crispr_ep_well_id ".$summary->crispr_ep_well_id;
-            my $crispr_ep_well = $self->model->schema->resultset('Well')->find({
-                id => $summary->crispr_ep_well_id
-            });
-            my $colony_count = $crispr_ep_well->search_related('well_colony_counts',{
-                colony_count_type_id => 'remaining_unstained_colonies'
-            })->first;
-            $data{colony_numbers} = $colony_count ? $colony_count->colony_count : undef;
-
-            my %ep_pick_damage;
-            foreach my $ep_pick_well_id (keys %num_wells){
-                my $ep_pick_well = $self->model->schema->resultset('Well')->find({
-                    id => $ep_pick_well_id,
-                });
-                #my $qc_well = $ep_pick_well->accepted_crispr_es_qc_well;
-                #next unless $qc_well;
-                foreach my $qc_well($ep_pick_well->crispr_es_qc_wells){
-                    # Skip QC wells that have not damage type
-                    # Is this the right thing to do?
-                    next unless $qc_well->crispr_damage_type_id;
-
-                    $ep_pick_damage{ $qc_well->crispr_damage_type_id }++;
-                }
-            }
-            DEBUG Dumper(%ep_pick_damage);
-            foreach my $damage_type (keys %ep_pick_damage){
-                $data{$damage_type} = $ep_pick_damage{$damage_type};
-            }
-
-            if( $data{ep_colonies_picked} and exists $data{frameshift} ){
-                my $efficiency = ($data{frameshift}/$data{ep_colonies_picked}) * 100;
-                $data{targeting_efficiency} = round($efficiency)."%";
-            }
-
-            $data{nhej_of_second_allele} = $data{frameshift} + $data{'in-frame'} + $data{mosaic};
-
-            #use hash slice to get all the values we want out in order
-            push @row, [ map { $_ } @data{ map { keys %{$_} } @{$self->column_map} } ];
-
+        my %num_wells;
+        my $num_accepted = 0;
+        for my $row ( @epds ) {
+            next unless $row->ep_pick_well_id;
+            $num_wells{ $row->ep_pick_well_id }++;
+            $num_accepted++ if $row->ep_pick_well_accepted;
         }
+
+        $data{ep_colonies_picked} = scalar( keys %num_wells );
+        $data{ep_colonies_accepted} = $num_accepted;
+
+        DEBUG "Finding colonly counts for crispr_ep_well_id ".$summary->crispr_ep_well_id;
+
+        my $crispr_ep_well = $crispr_ep_wells_by_id->{$summary->crispr_ep_well_id};
+        my $colony_count = $crispr_ep_well->search_related('well_colony_counts',{
+            colony_count_type_id => 'remaining_unstained_colonies'
+        })->first;
+        $data{colony_numbers} = $colony_count ? $colony_count->colony_count : undef;
+
+        # Count how many time each type of damage is seen in crispr QC
+        # of all the downstream EP_PICK wells
+        my %ep_pick_damage;
+        foreach my $ep_pick_well_id (keys %num_wells){
+            my $ep_pick_well = $self->model->schema->resultset('Well')->find({
+                id => $ep_pick_well_id,
+            });
+
+            foreach my $qc_well($ep_pick_well->crispr_es_qc_wells){
+                # Skip QC wells that have no damage type
+                # Is this the right thing to do?
+                next unless $qc_well->crispr_damage_type_id;
+
+                $ep_pick_damage{ $qc_well->crispr_damage_type_id }++;
+            }
+        }
+
+        foreach my $damage_type (keys %ep_pick_damage){
+            $data{$damage_type} = $ep_pick_damage{$damage_type};
+        }
+
+        # Calculate targeting efficiency from frameshift damage counts
+        if( $data{ep_colonies_picked} and exists $data{frameshift} ){
+            my $efficiency = ($data{frameshift}/$data{ep_colonies_picked}) * 100;
+            $data{targeting_efficiency} = round($efficiency)."%";
+        }
+
+        # Count all EP_PICKs where some damage was detected
+        $data{nhej_of_second_allele} = $data{frameshift} + $data{'in-frame'} + $data{mosaic};
+
+        # use hash slice to get all the values we want out in order
+        push @row, [ map { $_ } @data{ map { keys %{$_} } @{$self->column_map} } ];
+
     }
+
 
     return \@row;
 
