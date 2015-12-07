cdab8163bd21e63fbfbdf0efea6e02115b2f057c
af11_sanger <af11@sanger.ac.uk>
2015-12-07 13:18:53 +0000

    added PIQ expand and freeze back option

--- lib/LIMS2/WebApp/Controller/User/Barcodes.pm
+++ lib/LIMS2/WebApp/Controller/User/Barcodes.pm
@@ -8,6 +8,7 @@ use LIMS2::Model::Util::BarcodeActions qw(
     discard_well_barcode
     freeze_back_fp_barcode
     freeze_back_piq_barcode
+    piq_expand_freeze_back_barcode
     add_barcodes_to_wells
     upload_plate_scan
     send_out_well_barcode
@@ -349,6 +350,10 @@ sub freeze_back : Path( '/user/freeze_back' ) : Args(0){
         $c->stash->{template} = 'user/barcodes/piq_freeze_back.tt';
         $redirect_on_completion = $c->uri_for('/user/scan_barcode');
     }
+    elsif($type eq 'PIQ_EXPAND'){
+        $c->stash->{template} = 'user/barcodes/piq_expand_freeze_back.tt';
+        $redirect_on_completion = $c->uri_for('/user/scan_barcode');
+    }
 
     my $well;
     if($barcode){
@@ -386,6 +391,9 @@ sub freeze_back : Path( '/user/freeze_back' ) : Args(0){
         }
         elsif($c->req->param('freeze_back_type') eq 'FP'){
             $freeze_back_method = \&freeze_back_fp_barcode;
+        }
+        elsif($c->req->param('freeze_back_type') eq 'PIQ_EXPAND'){
+            $freeze_back_method = \&piq_expand_freeze_back_barcode;
         }
         else{
             die "Freeze back type not specified in form";
