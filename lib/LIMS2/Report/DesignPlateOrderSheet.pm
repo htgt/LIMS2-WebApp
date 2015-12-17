bbf37225750dc49f4532e2335c6e2aecd3d63191
Tiago Grego <tg6@sanger.ac.uk>
2015-12-17 11:52:39 +0000

    order sheet for fusion design

--- lib/LIMS2/Report/DesignPlateOrderSheet.pm
+++ lib/LIMS2/Report/DesignPlateOrderSheet.pm
@@ -117,6 +117,9 @@ sub _build_oligo_types {
     if ( $self->is_gibson ) {
         @oligo_types = qw( 5F 5R EF ER 3F 3R );
     }
+    elsif ( $self->is_fusion ) {
+        @oligo_types = qw( f5F U5 EF ER D3 f3R );
+    }
     else {
         @oligo_types = qw( G5 G3 U5 U3 D5 D3 );
     }
@@ -135,7 +138,7 @@ sub generate_design_plate_order_sheet_data {
 
     $self->build_base_report_data();
     $self->oligo_seq_data;
-    unless ( $self->is_gibson ) {
+    unless ( $self->is_gibson || $self->is_fusion ) {
         $self->bac_plate_data;
         $self->bac_list;
     }
@@ -160,7 +163,7 @@ sub build_base_report_data{
                 prefetch => { 'oligos' => 'loci' },
             }
         );
-        $self->set_design_well_bacs( $well, $parent_process) unless $self->is_gibson;
+        $self->set_design_well_bacs( $well, $parent_process) unless ($self->is_gibson || $self->is_fusion );
 
         my $oligo_seqs = $design->oligo_order_seqs;
         for my $oligo_type ( @{ $self->oligo_types } ) {
