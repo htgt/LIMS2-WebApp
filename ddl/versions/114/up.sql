b3712ea6e5ff0a3ae5f010b17849f81aea5db54c
Tiago Grego <tg6@sanger.ac.uk>
2016-03-16 14:33:18 +0000

    added sequencing_primers for tristan

--- ddl/versions/114/up.sql
+++ ddl/versions/114/up.sql
@@ -3,3 +3,16 @@ INSERT INTO process_types VALUES ('vector_cloning','Create Pre-Intermediate');
 
 INSERT INTO backbones (name,description,antibiotic_res,gateway_type) VALUES ('p1260_GERETY_T7_gRNA_vector_BSAI','Backbones sequence used in crispr vectors to facilitate the in vitro transcription of CRISPRs','KanR','');
 INSERT INTO backbones (name,description,antibiotic_res,gateway_type) VALUES ('p1260_GERETY_T7_gRNA_vector_BSAI_AUflip_extended_loop_backbone','Backbones sequence used in crispr vectors to facilitate the in vitro transcription of CRISPRs version 2','KanR','');
+
+INSERT INTO sequencing_primer_types VALUES ('Sp6_RP_T7_seq');
+INSERT INTO sequencing_primer_types VALUES ('gRNA_seqR1_T7');
+INSERT INTO sequencing_primer_types VALUES ('gRNA_seqR2_T7');
+INSERT INTO sequencing_primer_types VALUES ('FP_seqU6T7_F1');
+INSERT INTO sequencing_primer_types VALUES ('FP_seqU6T7_F2');
+INSERT INTO sequencing_primer_types VALUES ('seqEF1a_FP1');
+INSERT INTO sequencing_primer_types VALUES ('seqEF1a_FP2');
+INSERT INTO sequencing_primer_types VALUES ('EF1_R2');
+INSERT INTO sequencing_primer_types VALUES ('seqEF1a_FP3');
+INSERT INTO sequencing_primer_types VALUES ('seqEF1a_RP1');
+INSERT INTO sequencing_primer_types VALUES ('seqEF1a_RP3');
+INSERT INTO sequencing_primer_types VALUES ('seqEF1a_RP2');
