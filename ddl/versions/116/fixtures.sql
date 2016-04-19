17e93e7d22b668e972eddfaabb727a1b454cb439
Peter Keen <pk8@sanger.ac.uk>
2016-04-19 14:12:22 +0100

    Adding announcements to lims2

---
+++ ddl/versions/116/fixtures.sql
@@ -0,0 +1,5 @@
+INSERT INTO schema_versions(version) VALUES (116);
+
+INSERT INTO priorities(id) VALUES('high');
+INSERT INTO priorities(id) VALUES('normal');
+INSERT INTO priorities(id) VALUES('low');
