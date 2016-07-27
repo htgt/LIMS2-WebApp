c981c906d4442b60927abdb20d2dc76d0b2efa20
Peter Keen <pk8@sanger.ac.uk>
2016-07-27 10:38:58 +0100

    Selenium cross script attempt

--- script/lims2_js_test.pl
+++ script/lims2_js_test.pl
@@ -4,6 +4,8 @@ use Selenium::Firefox;
 use feature qw(say);
 use Getopt::Long;
 use Pod::Usage;
+use Data::Dumper;
+use JSON;
 #use Test::More tests => 5;
 #use Test::WWW::Jasmine;
 #use WWW::Selenium;
@@ -15,13 +17,14 @@ GetOptions(
     'log_out'   => \my $logged_out,
 );
 
-my @arg = @ARGV;
+my @path = @ARGV;
 
 #Open local webapp
 my $driver = Selenium::Firefox->new;
-$driver->get('t87-dev.internal.sanger.ac.uk:3232');
+$driver->get('t87-dev.internal.sanger.ac.uk:' . $ENV{LIMS2_WEBAPP_SERVER_PORT});
 say $driver->get_title();
 
+#Log into webapp
 unless ($logged_out) {
     my $elem = $driver->find_element_by_class('navbar-btn');
     $driver->mouse_move_to_location(element => $elem);
@@ -38,7 +41,24 @@ unless ($logged_out) {
     $driver->click;
     say $driver->get_title();
 }
-#Run page
+
+#Find tests
+if (@path) {
+
+}
+else {
+    my $base = $ENV{LIMS2_JS}
+        or die "LIMS2_JS not set";
+    my @results;
+    @results = check_folder($base, @results);
+    my $js = JSON->new;
+    my $encode = $js->allow_blessed->convert_blessed->encode($driver);
+    print Dumper $encode;
+    foreach my $file (@results) {
+        system($^X, $file, $encode);
+        #$driver->get('t87-dev.internal.sanger.ac.uk:' . $ENV{LIMS2_WEBAPP_SERVER_PORT});
+    }
+}
 
 #my $jasmine = Test::WWW::Jasmine->new(
 #    spec_file   => 'spec/tests/Test.js',
@@ -51,6 +71,24 @@ unless ($logged_out) {
 
 $driver->quit();
 
+sub check_folder {
+    my ($current_dir, @js_files) = @_;
+    
+    opendir my $dir, $current_dir or die "Cannot open directory: $!";
+    my @files = readdir $dir;
+    closedir $dir;
+
+    foreach my $file (@files) {
+        if ($file =~ /^\S*\.pm$/) {     # If JavaScript file, ignoring swps
+            push(@js_files, $current_dir . $file);
+        }
+        elsif ($file !~ /^\..*/) {      # If subdirectory
+            my $new_dir = $current_dir . $file . '/';
+            @js_files = check_folder($new_dir, @js_files);
+        }
+    }
+    return @js_files;
+}
 =head1 NAME
 
 lims2_js_test - Launch the Javascript testing suite for LIMS2
