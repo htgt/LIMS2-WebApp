3e39a0ddf24a0807c8d4ce8b539b9cb1a7630ecc
af11_sanger <af11@sanger.ac.uk>
2016-08-18 12:05:23 +0100

    use https for login page when configured using env variables

--- lib/LIMS2/WebApp.pm
+++ lib/LIMS2/WebApp.pm
@@ -1,4 +1,5 @@
 package LIMS2::WebApp;
+
 use Moose;
 use namespace::autoclean;
 
@@ -25,6 +26,7 @@ use Catalyst qw/
     Session::State::Cookie
     Authentication
     Authorization::Roles
+    RequireSSL
     /;
 
 extends 'Catalyst';
@@ -40,6 +42,15 @@ extends 'Catalyst';
 
 __PACKAGE__->config(
     name => 'LIMS2::WebApp',
+
+    require_ssl => {
+            https => $ENV{LIMS2_HTTPS_DOMAIN},
+            http => $ENV{LIMS2_HTTP_DOMAIN},
+            remain_in_ssl => 0,
+            no_cache => 0,
+            detach_on_redirect => 0,
+            disabled => $ENV{LIMS2_ENABLE_HTTPS} ? 0 : 1,
+    },
 
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
@@ -61,7 +72,8 @@ __PACKAGE__->config(
     'View::JSON' => { expose_stash => 'json_data' },
     'Plugin::Session' => {
         expires => 28800,                                # 8 hours
-        storage => $ENV{LIMS2_SESSION_STORE}
+        storage => $ENV{LIMS2_SESSION_STORE},
+        cookie_domain => $ENV{LIMS2_COOKIE_DOMAIN},
     },
     'static' => {
         include_path => [
@@ -77,6 +89,36 @@ __PACKAGE__->log( Log::Log4perl::Catalyst->new( $ENV{LIMS2_LOG4PERL_CONFIG}, aut
 
 # Start the application
 __PACKAGE__->setup();
+
+# Define additional method to get uri_for for the http domain
+# Use this to create a http link on a https page (e.g. the login page)
+sub http_uri_for{
+    my ($self, $path, @args) = @_;
+
+    my $uri = $self->uri_for($path, @args);
+    my $http_uri;
+
+    if($uri =~ /^http\:/){
+        $http_uri = $uri;
+    }
+    elsif($uri =~ /^https\:/){
+        if($self->config->{require_ssl}){
+            my $http = "http://".$self->config->{require_ssl}->{http};
+            my $https = quotemeta("https://".$self->config->{require_ssl}->{https});
+
+            $uri =~ s/$https/$http/;
+            $http_uri = URI->new($uri);
+        }
+        else{
+            die "Cannot convert https to http URI as require_ssl configuration was not found";
+        }
+    }
+    else{
+        die "Unrecognized URI scheme. Cannot convert to http: $uri";
+    }
+
+    return $http_uri;
+}
 
 =head1 NAME
 
