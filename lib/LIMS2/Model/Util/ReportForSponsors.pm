be69d660d613aca01c465db3e5c577450310ab5e
Peter Keen <pk8@sanger.ac.uk>
2016-09-06 10:19:10 +0100

    Revert "Front page changes"
    This reverts commit 9180f92f1db0ff67e9d3829c3697ce0b87403db1.

--- lib/LIMS2/Model/Util/ReportForSponsors.pm
+++ lib/LIMS2/Model/Util/ReportForSponsors.pm
@@ -1241,7 +1241,7 @@ sub genes {
         my $piq_pass_count = scalar @piq;
         my $toggle;
         if ($ep_count) {
-            $toggle = '-';
+            $toggle = 'y';
         }
         # push the data for the report
         push @genes_for_display, {
