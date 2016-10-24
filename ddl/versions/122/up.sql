8282eb498bbccf12cf826b823ce743d31e37d93a
Peter Keen <pk8@sanger.ac.uk>
2016-10-24 12:13:53 +0100

    schema change

--- ddl/versions/122/up.sql
+++ ddl/versions/122/up.sql
@@ -1,1 +1,5 @@
-ALTER TABLE users ADD COLUMN key char(304);
+CREATE TABLE user_api_keys (
+    user_id INT PRIMARY KEY REFERENCES users(id) NOT NULL,
+    access_key CHAR(36) NOT NULL,
+    secret_key CHAR(304) NOT NULL,
+);
