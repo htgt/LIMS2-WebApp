23292f307de8bb050fef4fabcff5c80664e860d3
Peter Keen <pk8@sanger.ac.uk>
2017-07-24 16:02:46 +0100

    Parent plate fixtures

--- ddl/versions/132/fixtures.sql
+++ ddl/versions/132/fixtures.sql
@@ -1,11 +1,10 @@
---INSERT INTO schema_versions(version) VALUES (132);
+INSERT INTO schema_versions(version) VALUES (132);
 
---INSERT INTO plate_types VALUES ('MISEQ','MiSEQ QC Plate');
+INSERT INTO plate_types VALUES ('MISEQ','MiSEQ QC Plate');
 
---INSERT INTO process_types VALUES ('miseq_oligo','Create miseq plate with oligo for HDR events');
---INSERT INTO process_types VALUES ('miseq_vector','Create miseq plate with vector for HDR events');
---INSERT INTO process_types VALUES ('miseq_no_template','Create miseq plate for analysis of NHEJ events only');
-
+INSERT INTO process_types VALUES ('miseq_oligo','Create miseq plate with oligo for HDR events');
+INSERT INTO process_types VALUES ('miseq_vector','Create miseq plate with vector for HDR events');
+INSERT INTO process_types VALUES ('miseq_no_template','Create miseq plate for analysis of NHEJ events only');
 
 DO $$
 DECLARE
