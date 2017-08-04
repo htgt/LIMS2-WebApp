a914be153123632ee4cc722f88a9613cf918d6dc
Peter Keen <pk8@sanger.ac.uk>
2017-08-04 14:27:00 +0100

    Miseq_007 history file

--- ddl/versions/133/fixtures.sql
+++ ddl/versions/133/fixtures.sql
@@ -36,20 +36,21 @@ BEGIN
     END LOOP;
 
 -- 384 
-
-    _fp := 'Miseq_004_FP';
-    INSERT INTO plates (name, type_id, created_by_id, species_id) VALUES (_fp, 'FP', 432, 'Human');
-    SELECT id INTO _id FROM plates WHERE name = _fp;
-    RAISE NOTICE 'PlateID: %', _id;
-    FOR ro IN 65..80 LOOP
-        _row := chr(ro);
-        FOR co IN 1..24 LOOP
-            _col := co;
-            IF (co < 10) THEN
-                _col := '0' || _col;
-            END IF;
-            _well := _row || _col;
-            INSERT INTO wells (plate_id, name, created_by_id) VALUES (_id, _well, 432);
+    FOR fp IN 4..6 LOOP
+        _fp := 'Miseq_00' || fp || '_FP';
+        INSERT INTO plates (name, type_id, created_by_id, species_id) VALUES (_fp, 'FP', 432, 'Human');
+        SELECT id INTO _id FROM plates WHERE name = _fp;
+        RAISE NOTICE 'PlateID: %', _id;
+        FOR ro IN 65..80 LOOP
+            _row := chr(ro);
+            FOR co IN 1..24 LOOP
+                _col := co;
+                IF (co < 10) THEN
+                    _col := '0' || _col;
+                END IF;
+                _well := _row || _col;
+                INSERT INTO wells (plate_id, name, created_by_id) VALUES (_id, _well, 432);
+            END LOOP;
         END LOOP;
     END LOOP;
     RETURN;
