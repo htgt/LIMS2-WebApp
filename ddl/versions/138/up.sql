bce1d612d17beb68cdb965ca374e5da357a1f0f1
Peter Keen <pk8@sanger.ac.uk>
2018-02-21 12:48:51 +0000

    reusable primer params

---
+++ ddl/versions/138/up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE miseq_design_presets (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    min_gc INTEGER,
+    max_gc INTEGER,
+    opt_gc INTEGER,
+    min_mt INTEGER,
+    max_mt INTEGER,
+    opt_mt INTEGER,
+);
+
+CREATE TABLE miseq_primer_parameters (
+    id SERIAL PRIMARY KEY,
+    internal boolean NOT NULL,
+    min_length INTEGER,
+    max_length INTEGER,
+    opt_length INTEGER,
+);
