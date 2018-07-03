8a3071f38a42541fadb1304a60b1dca33aa1b9c2
Gerasimos Vandoros <gv3@sanger.ac.uk>
2018-07-03 13:49:20 +0100

    Regenerated schema

--- lib/LIMS2/Model/Schema/Result/Design.pm
+++ lib/LIMS2/Model/Schema/Result/Design.pm
@@ -265,6 +265,21 @@ __PACKAGE__->has_many(
   { cascade_copy => 0, cascade_delete => 0 },
 );
 
+=head2 hdr_templates
+
+Type: has_many
+
+Related object: L<LIMS2::Model::Schema::Result::HdrTemplate>
+
+=cut
+
+__PACKAGE__->has_many(
+  "hdr_templates",
+  "LIMS2::Model::Schema::Result::HdrTemplate",
+  { "foreign.design_id" => "self.id" },
+  { cascade_copy => 0, cascade_delete => 0 },
+);
+
 =head2 nonsense_design_crispr
 
 Type: belongs_to
@@ -381,8 +396,8 @@ __PACKAGE__->belongs_to(
 );
 
 
-# Created by DBIx::Class::Schema::Loader v0.07022 @ 2018-07-03 11:49:52
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:X91lqhWUlzyA8b8UIfOOIw
+# Created by DBIx::Class::Schema::Loader v0.07022 @ 2018-07-03 13:49:09
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gTgbTVIPyxlnr3tZBKgQjw
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
