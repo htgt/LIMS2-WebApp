aeb62cf61729e1b9f9dd142d8b33f4eba0b157ed
jmrein <jr27@sanger.ac.uk>
2018-12-20 12:24:15 +0000

    Directly create CRISPResso jobs from LIMS2
    Rather than spawning them from within a bsub, as we can't set those as a
    dependency for the move script.

--- lib/LIMS2/WebApp/Controller/User/Miseq.pm
+++ lib/LIMS2/WebApp/Controller/User/Miseq.pm
@@ -1,6 +1,7 @@
 package LIMS2::WebApp::Controller::User::Miseq;
 use Moose;
 use namespace::autoclean;
+use Bio::Perl qw/revcom_as_string/;
 use Data::UUID;
 use Data::Dumper;
 use File::Spec::Functions qw/catfile/;
@@ -22,9 +23,8 @@ has file_api => (
 );
 
 sub _build_file_api {
-    return HTGT::QC::Util::FileAccessServer->new({
-            file_api_url => $ENV{FILE_API_URL},
-        });
+    return HTGT::QC::Util::FileAccessServer->new(
+        { file_api_url => $ENV{FILE_API_URL}, } );
 }
 
 has farm_job_runner => (
@@ -34,7 +34,7 @@ has farm_job_runner => (
 );
 
 sub _build_farm_job_runner {
-    return WebAppCommon::Util::FarmJobRunner->new({ dry_run => 0 });
+    return WebAppCommon::Util::FarmJobRunner->new( { dry_run => 0 } );
 }
 
 has spreadsheet_columns => (
@@ -51,16 +51,16 @@ has spreadsheet_columns => (
 
 sub _build_spreadsheet_columns {
     tie my %columns, 'Tie::IxHash';
-    $columns{Experiment} = qr/.+/xms; #anything goes, but must be something
-    $columns{Gene}       = qr/^[A-Z0-9]+ # start with a gene symbol
+    $columns{Experiment} = qr/.+/xms;    #anything goes, but must be something
+    $columns{Gene} = qr/^[A-Z0-9]+ # start with a gene symbol
                         (?:_.*)? # can be followed by an underscore and whatever
-                        $/xms; 
-    $columns{Crispr}     = qr/^[ACGT]{20}$/xms;
-    $columns{Strand}     = qr/^[+-]$/xms;
-    $columns{Amplicon}   = qr/^[ACGT]+$/ixms;
-    $columns{min_index}  = qr/^\d+$/xms;
-    $columns{max_index}  = qr/^\d+$/xms;
-    $columns{HDR}        = qr/^[ACGT]*$/ixms;
+                        $/xms;
+    $columns{Crispr}    = qr/^[ACGT]{20}(?:,[ACGT]{20})*$/ixms;
+    $columns{Strand}    = qr/^[+-]$/xms;
+    $columns{Amplicon}  = qr/^[ACGT]+$/ixms;
+    $columns{min_index} = qr/^\d+$/xms;
+    $columns{max_index} = qr/^\d+$/xms;
+    $columns{HDR}       = qr/^[ACGT]*$/ixms;
     return \%columns;
 }
 
@@ -88,17 +88,18 @@ sub _validate_columns {
             push @missing, $column;
         }
     }
-    if ( @missing ) {
-        die 'Missing required columns: ' . join(q/, /, @missing);
+    if (@missing) {
+        die 'Missing required columns: ' . join( q/, /, @missing );
     }
     return;
 }
 
 sub _validate_value {
     my ( $row, $key, $validator, $line ) = @_;
-    my $value = $row->{$key};
+    my $value = $row->{$key} // q//;
     if ( not $value =~ $validator ) {
-        my $line_text = exists $row->{_rownum} ? " on line $row->{_rownum}" : q//;
+        my $line_text =
+          exists $row->{_rownum} ? " on line $row->{_rownum}" : q//;
         die "'$value' is not a valid value for $key$line_text";
     }
     return;
@@ -114,15 +115,15 @@ sub _validate_values {
 
 sub _read_csv {
     my ( $self, $fh ) = @_;
-    my $csv = Text::CSV->new({ binary => 1 });
+    my $csv = Text::CSV->new( { binary => 1 } );
     my $headers = $csv->getline($fh);
     $self->_validate_columns($headers);
     $csv->column_names( @{$headers} );
-    my @rows = ();
+    my @rows   = ();
     my $rownum = 1;
     while ( my $row = $csv->getline_hr($fh) ) {
         $row->{_rownum} = ++$rownum;
-        $self->_validate_values( $row );
+        $self->_validate_values($row);
         push @rows, $row;
     }
     return \@rows;
@@ -130,7 +131,8 @@ sub _read_csv {
 
 sub _read_file {
     my ( $self, $file ) = @_;
-    open my $fh, '<:encoding(utf8)', $file or die "Could not open spreadsheet: $!";
+    open my $fh, '<:encoding(utf8)', $file
+      or die "Could not open spreadsheet: $!";
     my $exps = $self->_read_csv($fh);
     close $fh or die "Could not close spreadsheet: $!";
     return $exps;
@@ -138,12 +140,12 @@ sub _read_file {
 
 sub _write_file {
     my ( $self, $exps ) = @_;
-    my $csv = Text::CSV->new({ binary => 1, sep_char => q/,/, eol => "\n" });
+    my $csv = Text::CSV->new( { binary => 1, sep_char => q/,/, eol => "\n" } );
     open my $fh, '>', \my $buffer or die "Could not write spreadsheet: $!";
     my @columns = $self->columns;
     $csv->print( $fh, \@columns );
     foreach my $exp ( @{$exps} ) {
-        $csv->print( $fh, [map { $exp->{$_} } @columns] );
+        $csv->print( $fh, [ map { $exp->{$_} } @columns ] );
     }
     close $fh or die "Could not close spreadsheet: $!";
     return $buffer;
@@ -153,28 +155,29 @@ sub _get_plates {
     my ( $context, $samples, $experiments ) = @_;
     my ( $num_rows, $num_cols, $num_plates ) = ( 0, 0, 0 );
     my %barcodes = ();
-    my $a = ord('A');
+    my $a        = ord('A');
     foreach my $sample ( @{$samples} ) {
         my ( $name, $row, $col, $plate ) =
-            $sample->sample_id =~ /^(([A-Z])([0-9]+)_([0-9]+))/xms;
+          $sample->sample_id =~ /^(([A-Z])([0-9]+)_([0-9]+))/xms;
         next if not $name;
-        $num_rows = max $num_rows, (ord($row) - $a);
-        $num_cols = max $num_cols, $col;
+        $num_rows = max $num_rows, ( ord($row) - $a );
+        $num_cols   = max $num_cols,   $col;
         $num_plates = max $num_plates, $plate;
         $barcodes{$name} = $sample->name;
     }
-    my @plates   = ();
+    my @plates = ();
     foreach my $p ( 1 .. $num_plates ) {
         my @plate = ();
         foreach my $r ( 1 .. $num_rows ) {
             my @row = ();
             foreach my $c ( 1 .. $num_cols ) {
-                my $name = sprintf("%s%02d_%d", chr($r + $a - 1), $c, $p);
+                my $name = sprintf( "%s%02d_%d", chr( $r + $a - 1 ), $c, $p );
                 my $barcode = $barcodes{$name} // 0;
                 my @exps = ();
                 foreach my $exp ( @{$experiments} ) {
-                    if ( ($barcode >= $exp->{min_index})
-                        && ($barcode <= $exp->{max_index}) ) {
+                    if (   ( $barcode >= $exp->{min_index} )
+                        && ( $barcode <= $exp->{max_index} ) )
+                    {
                         push @exps, $exp;
                     }
                 }
@@ -183,7 +186,7 @@ sub _get_plates {
                     barcode => $barcode,
                     exps    => \@exps,
                 };
-                push @row, $well; 
+                push @row, $well;
             }
             push @plate, \@row;
         }
@@ -192,73 +195,113 @@ sub _get_plates {
     return \@plates;
 }
 
-
+#this mostly exists to stop it breaking when running with dry_run
 sub _get_dependency {
     my ( $self, @jobs ) = @_;
     return $self->farm_job_runner->dry_run ? [0] : \@jobs;
 }
 
+sub _submit_crispresso {
+    my ( $self, $exp, $path, $crispresso_script, @dependencies ) = @_;
+    my $id = $exp->{Experiment};
+    my @crisprs = split q/,/, uc( $exp->{Crispr} );
+    if ( $exp->{Strand} eq '-' ) {
+        foreach my $crispr (@crisprs) {
+            $crispr = revcom_as_string($crispr);
+        }
+    }
+    return $self->farm_job_runner->submit(
+        {
+            name => sprintf( 'cp_%s[%d-%d]',
+                $id, $exp->{min_index}, $exp->{max_index} ),
+            cwd          => $path,
+            out_file     => "cp_$id.%J.%I.out",
+            err_file     => "cp_$id.%J.%I.err",
+            dependencies => $self->_get_dependency(@dependencies),
+            cmd          => [
+                $crispresso_script,
+                '-g' => ( join q/,/, @crisprs ),
+                '-a' => $exp->{Amplicon},
+                '-n' => $id,
+            ],
+        }
+    );
+}
+
 sub _process {
     my ( $self, $c ) = @_;
 
-    my $bs = LIMS2::Model::Util::BaseSpace->new;
-    my $jobid = Data::UUID->new->create_str;
-    my $path = catfile($ENV{LIMS2_MISEQ_PROCESS_PATH}, $jobid);
+    my $bs     = LIMS2::Model::Util::BaseSpace->new;
+    my $jobid  = Data::UUID->new->create_str;
+    my $plate  = $c->request->param('plate');
+    my $walkup = $c->request->param('walkup');
+    my $path =
+      catfile( $ENV{LIMS2_MISEQ_PROCESS_PATH}, join( q/_/, $plate, $jobid ) );
     my $scripts = $ENV{LIMS2_MISEQ_SCRIPTS_PATH};
 
-    my $plate = $c->request->param('plate');
-    my $walkup = $c->request->param('walkup');
-    my $experiments = $self->_read_file($c->request->upload('spreadsheet')->tempname);
+    my $experiments =
+      $self->_read_file( $c->request->upload('spreadsheet')->tempname );
     $c->stash->{experiments} = $experiments;
 
-    die "'$plate' is not a valid name for a plate" if not $path =~ m/^\w+$/;
-    
-    my $destination = catfile($ENV{LIMS2_MISEQ_STORAGE_PATH}, $plate);
+    die "'$plate' is not a valid name for a plate"   if not $plate  =~ m/^\w+$/;
+    die "'$walkup' is not a valid walkup identifier" if not $walkup =~ m/^\d+$/;
+
+    my $destination = catfile( $ENV{LIMS2_MISEQ_STORAGE_PATH}, $plate );
+    my $date = localtime->strftime('%d-%m-%Y');
+    my $raw_dest =
+      catfile( $ENV{LIMS2_MISEQ_RAW_PATH}, "${plate}_BS${walkup}_$date" );
     if ( not mkdir $destination ) {
         $c->log->debug("Cannot store $plate: $!");
         die "Cannot store $plate";
     }
-    
+    if ( not mkdir $raw_dest ) {
+        $c->log->debug("Cannot store raw data: $!");
+        die 'Cannot store raw data';
+    }
     my $project = LIMS2::Model::Util::BaseSpace->new->project($walkup);
     my @samples = $project->samples;
-    my $plates = _get_plates($c, \@samples, $experiments);
-    $c->stash->{plates} = $plates; 
+    my $plates  = _get_plates( $c, \@samples, $experiments );
+    $c->stash->{plates} = $plates;
 
     $c->stash->{job_id} = $jobid;
     $self->file_api->make_dir($path);
-    $self->file_api->post_file_content(catfile($path, 'summary.csv'),
-        $self->_write_file($experiments)); 
-    $self->file_api->post_file_content(catfile($path, 'samples.txt'), 
-        join("\n", map { $_->id } @samples));
+    $self->file_api->post_file_content( catfile( $path, 'summary.csv' ),
+        $self->_write_file($experiments) );
+    $self->file_api->post_file_content(
+        catfile( $path, 'samples.txt' ),
+        join( "\n", map { $_->id } @samples )
+    );
 
-    my $numsamples = scalar(@samples);
-    my $downloader = catfile($scripts, 'bjob_download_basespace.sh');
-    my $download_job = $self->farm_job_runner->submit({
+    my $numsamples   = scalar(@samples);
+    my $downloader   = catfile( $scripts, 'bjob_download_basespace.sh' );
+    my $download_job = $self->farm_job_runner->submit(
+        {
             name     => "dl_${jobid}\[1-${numsamples}]%10",
             cwd      => $path,
             out_file => 'dl.%J.%I.out',
             err_file => 'dl.%J.%I.err',
-            cmd      => [$downloader, $bs->{token}],
-        });
-    my $crispresso_job = $self->farm_job_runner->submit({
-            name     => 'bsub_crispresso',
-            cwd      => $path,
-            out_file => 'cp.%J.out',
-            err_file => 'cp.%J.err',
-            cmd      => [catfile($scripts, 'bsub_crispresso_jobs.sh'),
-                '--samples="summary.csv"',
-                '--dir=.',
-            ],
-            dependencies => $self->_get_dependency($download_job),
-        });
-    my $move_job = $self->farm_job_runner->submit({
+            cmd      => [ $downloader, $bs->{token} ],
+        }
+    );
+    my $crispresso_script = catfile( $scripts, 'bjob_crispresso.sh' );
+    my @crispresso_jobs = map {
+        $self->_submit_crispresso( $_, $path,
+            $crispresso_script, $download_job )
+    } @{$experiments};
+    my $move_job = $self->farm_job_runner->submit(
+        {
             name     => 'move_miseq_data',
             cwd      => $path,
             out_file => 'mv.%J.out',
             err_file => 'mv.%J.err',
-            cmd      => [catfile($scripts, 'move_miseq_data.sh'), $destination],
-            dependencies => $self->_get_dependency($crispresso_job),
-        });
+            cmd      => [
+                catfile( $scripts, 'move_miseq_data.sh' ),
+                '-p', $destination, '-r', $raw_dest
+            ],
+            dep_type     => 'ended',
+            dependencies => $self->_get_dependency(@crispresso_jobs),
+        }
+    );
 
     $c->stash->{farm_job} = $move_job;
     return;
@@ -267,16 +310,21 @@ sub _process {
 sub submit : Path('/user/miseq/submit') : Args(0) {
     my ( $self, $c ) = @_;
     try {
-        foreach my $var ( qw/PROCESS_PATH SCRIPTS_PATH STORAGE_PATH/ ) {
-            die "LIMS2_MISEQ_$var is not set" if not exists $ENV{"LIMS2_MISEQ_$var"};
+        foreach my $var (qw/PROCESS_PATH SCRIPTS_PATH STORAGE_PATH RAW_PATH/) {
+            die "LIMS2_MISEQ_$var is not set"
+              if not exists $ENV{"LIMS2_MISEQ_$var"};
         }
-        _check_params($c->request,
-            'plate'  => 'You must specify which MiSeq plate was sent',
-            'walkup' => 'You must specify which MiSeq walkup contained the data',
+        _check_params(
+            $c->request,
+            'plate' => 'You must specify which MiSeq plate was sent',
+            'walkup' =>
+              'You must specify which MiSeq walkup contained the data',
             'spreadsheet' => {
-                type    => 'upload',
-                message => 'You must upload a CSV containing the MiSeq manifest',
-            });
+                type => 'upload',
+                message =>
+                  'You must upload a CSV containing the MiSeq manifest',
+            }
+        );
         $self->_process($c);
     }
     catch {
@@ -288,14 +336,14 @@ sub submit : Path('/user/miseq/submit') : Args(0) {
 sub import : Path('/user/miseq/import') : Args(0) {
     my ( $self, $c ) = @_;
     my $model = $c->model('Golgi');
-    my $bs = LIMS2::Model::Util::BaseSpace->new;
-    my @plates = $model->schema->resultset('Plate')->search(
-        { type_id => 'MISEQ' },
-        { columns => [qw/id name/] },
-    );
+    my $bs    = LIMS2::Model::Util::BaseSpace->new;
+    my @plates =
+      $model->schema->resultset('Plate')
+      ->search( { type_id => 'MISEQ' }, { columns => [qw/id name/] }, );
     try {
-        $c->stash->{plates} = [ sort { $b->id <=> $a->id } @plates ]; 
-        $c->stash->{projects} = [ sort { $b->created cmp $a->created } $bs->projects ];
+        $c->stash->{plates} = [ sort { $b->id <=> $a->id } @plates ];
+        $c->stash->{projects} =
+          [ sort { $b->created cmp $a->created } $bs->projects ];
     }
     catch {
         $c->stash->{error_msg} = $_;
