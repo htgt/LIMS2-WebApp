95fc11e938784ead229d008255b2fcd2e6107f85
Peter Keen <pk8@sanger.ac.uk>
2020-02-05 11:58:48 +0000

    Added retro rna seq env var

--- lib/LIMS2/Model/Util/ImportCrispressoQC.pm
+++ lib/LIMS2/Model/Util/ImportCrispressoQC.pm
@@ -41,7 +41,7 @@ sub _build_file_api {
 sub construct_miseq_path {
     my ($miseq, $index, $exp, $file) = @_;
 
-    my $path = $ENV{'LIMS2_RNA_SEQ'} .
+    my $path = $ENV{'WAREHOUSE_RNA_SEQ'} .
         '/' . $miseq .
         '/S' . $index . '_exp' . $exp .
         '/' . $file;
