51496f458af369066cbd10cf4698039ad4499b48
Beth Sampher <bs15@sanger.ac.uk>
2020-07-23 12:24:06 +0000

    Improved invalid/inexistent well name logging #1

--- lib/LIMS2/WebApp/Controller/API/PointMutation.pm
+++ lib/LIMS2/WebApp/Controller/API/PointMutation.pm
@@ -157,10 +157,10 @@ sub get_experiment_data {
     foreach my $miseq_well_exp (@miseq_well_exps) {
         my $miseq_well_exp_hash = $c->model('Golgi')->schema->resultset('MiseqWellExperiment')->find({ id => $miseq_well_exp->id })->as_hash;
         my $well_name = $miseq_well_exp_hash->{well_name};
-        if ($well_name eq '') {
-            $c->log->debug('Warning: well name not found');
-        }
         my $index = convert_well_name_to_index($well_name);
+        if (! $index) {
+            $c->log->debug("Warning: well name not found for id $miseq_well_exp_hash->{id}");
+        }
         my @alleles_freq_data;
         @alleles_freq_data = split("\n", get_frequency_data($c, $miseq_well_exp_hash));
         if (! @alleles_freq_data) {
