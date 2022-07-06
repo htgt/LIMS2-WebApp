423a0958ea022ecdb57910444a3274985f1b946a
Beth Sampher <bs15@sanger.ac.uk>
2022-07-06 11:12:33 +0100

    Add SW48 cell line and Nneka to lab heads

---
+++ ddl/versions/158/fixtures.sql
@@ -0,0 +1,4 @@
+INSERT INTO cell_lines (name, description, species_id) VALUES ('SW48', 'Cells isolated from the large intestine of an 82-year-old, Caucasian, female grade IV Dukes C colorectal cancer patient', 'Human');
+INSERT INTO cell_line_repositories VALUES ('ATCC');
+INSERT INTO cell_line_external (cell_line_id, remote_identifier, repository, url) VALUES (27, 'CCL-231', 'ATCC', 'https://www.atcc.org/products/ccl-231');
+INSERT INTO lab_heads VALUES ('Nneka Anyanwu');
