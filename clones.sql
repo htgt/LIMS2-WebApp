e1a7b302f7e9a16e5318142a32190b4e891e2c12
ac64 <ac64@sanger.ac.uk>
2023-01-11 21:13:31 +0000

    Get wells in parent plates

--- clones.sql
+++ clones.sql
@@ -3,5 +3,12 @@ WITH piq_plates AS (
      ),
      wells_in_piq_plates AS (
         SELECT wells.id AS id FROM wells JOIN piq_plates ON wells.plate_id = piq_plates.id
+     ),
+     wells_in_parent_plates AS (
+        SELECT process_output_well.well_id AS id
+        FROM process_output_well
+        JOIN processes ON processes.id = process_output_well.process_id
+        JOIN process_input_well ON processes.id = process_input_well.process_id
+        WHERE process_input_well.well_id IN (SELECT id FROM wells_in_piq_plates)
      )
-SELECT * FROM wells_in_piq_plates;
+SELECT * FROM wells_in_parent_plates;
