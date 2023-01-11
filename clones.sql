b58885bfc548225d655f6e6672d7e42519983a18
ac64 <ac64@sanger.ac.uk>
2023-01-11 21:55:29 +0000

    Filter for wells in freeze plates

--- clones.sql
+++ clones.sql
@@ -10,5 +10,15 @@ WITH piq_plates AS (
         JOIN processes ON processes.id = process_output_well.process_id
         JOIN process_input_well ON processes.id = process_input_well.process_id
         WHERE process_input_well.well_id IN (SELECT id FROM wells_in_piq_plates)
+     ),
+     wells_in_freeze_plates AS (
+        SELECT wells.id AS well_id,
+               wells.name AS well_name,
+               plates.id AS plate_id,
+               plates.name AS plate_name
+        FROM wells_in_parent_plates
+        JOIN wells ON wells.id = wells_in_parent_plates.id
+        JOIN plates ON plates.id = wells.plate_id
+        WHERE plates.type_id = 'FP'
      )
-SELECT * FROM wells_in_parent_plates;
+SELECT * FROM wells_in_freeze_plates;
