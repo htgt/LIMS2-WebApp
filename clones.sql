7680d6b816e6c6b37fed728e507b1c24fe902f81
ac64 <ac64@sanger.ac.uk>
2023-01-11 20:06:06 +0000

    Simplify by only returning ids

--- clones.sql
+++ clones.sql
@@ -1,7 +1,7 @@
 WITH piq_plates AS (
-        SELECT * FROM plates WHERE type_id = 'PIQ'
+        SELECT id FROM plates WHERE type_id = 'PIQ'
      ),
      wells_in_piq_plates AS (
-        SELECT * FROM wells JOIN piq_plates ON wells.plate_id = piq_plates.id
+        SELECT wells.id AS id FROM wells JOIN piq_plates ON wells.plate_id = piq_plates.id
      )
 SELECT * FROM wells_in_piq_plates;
