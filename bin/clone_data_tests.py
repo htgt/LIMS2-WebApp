18b8c5b5d051ee7ce98e3b7e0e6cb13b7959ba71
ac64 <ac64@sanger.ac.uk>
2023-01-20 17:52:34 +0000

    Add error objects.
    Allows us to classify the differnt types of errors.

--- bin/clone_data_tests.py
+++ bin/clone_data_tests.py
@@ -2,7 +2,20 @@ from collections import namedtuple
 from csv import DictReader
 from time import sleep
 
-from requests import get, ConnectionError
+from requests import get, ConnectionError, JSONDecodeError
+
+class CloneDataError(object):
+    """Base error.
+
+    Errors in this case are not exceptions.
+    """
+
+class Non200HTMLStatus(CloneDataError):
+    def __init__(self, status_code):
+        self.status_code = status_code
+
+class NotJSONData(CloneDataError):
+    pass
 
 # Check that the server is up and running.
 for t in range(60):
@@ -15,7 +28,7 @@ for t in range(60):
     else:
         break
 
-Result = namedtuple("Result", ["clone_name", "response_status", "json_data"])
+Result = namedtuple("Result", ["clone_name", "response_status", "json_data", "error"])
 
 with open("list_of_clones_12_01_23.tsv", newline='') as f:
     clones = DictReader(f, delimiter='\t')
@@ -27,7 +40,23 @@ with open("list_of_clones_12_01_23.tsv", newline='') as f:
             f"http://localhost:8081/public_reports/well_genotyping_info/{clone['plate_name']}/{clone['well_name']}",
             headers={"accept": "application/json"},
         )
-        results.append(Result(clone_name=clone["plate_name"]+"_"+clone["well_name"], response_status=response.status_code, json_data=json_data))
+        if response.status_code == 200:
+            try:
+                json_data = response.json()
+            except JSONDecodeError:
+                error = NotJSONData()
+            else:
+                error = None
+        else:
+            json_data = None
+        results.append(
+            Result(
+                clone_name=clone["plate_name"]+"_"+clone["well_name"],
+                response_status=response.status_code,
+                json_data=json_data,
+                error=error,
+            )
+        )
 
 print("Total number of clones: ", len(results))
 print("Total number of 'good' clones: ", len([result for result in results if result.response_status == 200]))
