39911add5ee57acbe9aaadd1cc0a0287e09008a8
ac64 <ac64@sanger.ac.uk>
2023-01-31 18:06:23 +0000

    Add Plate and Well class using reflection.

--- bin/clone_data_tests/create_data_graphs.py
+++ bin/clone_data_tests/create_data_graphs.py
@@ -1,9 +1,19 @@
 from sys import argv
 
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, MetaData
+from sqlalchemy.ext.automap import automap_base
 
 data_base_details = argv[1]
 engine = create_engine(f"postgresql://{data_base_details}")
+
+metadata = MetaData()
+metadata.reflect(engine, only=["plates", "wells"])
+
+Base = automap_base(metadata=metadata)
+Base.prepare()
+
+Plate = Base.classes.plates
+Well = Base.classes.wells
 
 with engine.connect() as conn:
     results = conn.execute(text("SELECT * FROM plates"))
