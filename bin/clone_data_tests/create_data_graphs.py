bd0d3f1b09bc457347c23710bccce1b847c1a9e5
ac64 <ac64@sanger.ac.uk>
2023-02-02 17:32:46 +0000

    Add function to get 'next' wells from fp well.
    This currently gets all the wells, not just the PIQ wells, which is what
    we ultimately want.
    
    My understanding of how sql alchemy works is still quite bad at this
    point. I'm basically relying on manual testing of the results to assure
    myself that this is working right.

--- bin/clone_data_tests/create_data_graphs.py
+++ bin/clone_data_tests/create_data_graphs.py
@@ -3,23 +3,39 @@ from collections import namedtuple
 
 from sqlalchemy import create_engine, select, text, MetaData, tuple_
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import joinedload, Session
+from sqlalchemy.orm import joinedload, relationship, Session
+
 
 engine = None
 
 Plate = None
+Process = None
 Well = None
 
 
 def init(data_base_details):
-    global engine, Plate, Well
+    global engine, Plate, Process, Well
     engine = create_engine(f"postgresql://{data_base_details}")
     metadata = MetaData()
-    metadata.reflect(engine, only=["plates", "wells"])
+    metadata.reflect(engine, only=["plates", "process_input_well", "process_output_well", "processes", "wells"])
     Base = automap_base(metadata=metadata)
+    wells = metadata.tables["wells"]
+    processes = metadata.tables["processes"]
+    process_input_well = metadata.tables["process_input_well"]
+    process_output_well = metadata.tables["process_output_well"]
+    well_to_well_join = process_input_well.join(processes, process_input_well.c.process_id == processes.c.id).join(process_output_well, process_output_well.c.process_id == processes.c.id)
+    class Well(Base):
+        __tablename__ = "wells"
+        next_wells = relationship(
+            "Well",
+            secondary=well_to_well_join,
+            primaryjoin=wells.c.id == process_input_well.c.well_id,
+            secondaryjoin=process_output_well.c.well_id == wells.c.id,
+        )
     Base.prepare()
     Plate = Base.classes.plates
-    Well = Base.classes.wells
+    Process = Base.classes.processes
+    return metadata
 
 
 def get_clones():
@@ -42,9 +58,15 @@ def get_fp_wells_from_clones(clones):
             .where(tuple_(Plate.name, Well.name).in_(clones))
             .options(joinedload(Well.plates))
         )
-
-        wells = [r.wells for r in results]
+        wells = [r.Well for r in results]
     return wells
+
+
+def get_piq_wells_from_fp_well(fp_well):
+    with Session(engine) as session:
+        session.add(fp_well)
+        next_wells = fp_well.next_wells
+    return next_wells
 
 
 if __name__ == "__main__":
@@ -61,5 +83,12 @@ if __name__ == "__main__":
     # Should be one well for each clone.
     assert len(fp_wells) == expected_number_of_clones, f"Expected {expected_number_of_clones} freeze plate wells,found {len(fp_wells)}."
     # Check that known clone, HUPFP0085A1_C10 from LIMS-46 tests, is in returned data.
-    wells_for_clone_HUPFP0085A1_C10 = list(filter(lambda well: well.name == "C10" and well.plates.name == "HUPFP0085A1", fp_wells))
-    assert len(wells_for_clone_HUPFP0085A1_C10) == 1, f"Found {len(wells_for_clone_HUPFP0085A1_C10)} wells containing clone HUPFP0085A1_C10"
+    try:
+        test_fp_well = list(filter(
+            lambda well: well.name == "C10" and well.plates.name == "HUPFP0085A1", fp_wells
+        ))[0]
+    except IndexError:
+        assert False, "Cannot find well for HUPFP0085A1_C10"
+
+    test_piq_wells = get_piq_wells_from_fp_well(test_fp_well)
+    assert len(test_piq_wells) == 1, f"Found {len(test_piq_wells)}"
