4665aa5a5ef4fc37b8b60b34c3e50e5f86225afe
ac64 <ac64@sanger.ac.uk>
2023-02-02 19:57:00 +0000

    Prefetch plate data.
    We also add an assert which checks the data is in the right form.

--- bin/clone_data_tests/create_data_graphs.py
+++ bin/clone_data_tests/create_data_graphs.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 
 from sqlalchemy import create_engine, select, text, MetaData, tuple_
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import joinedload, Session
 
 Clone = namedtuple("Clone", ["plate", "well"])
 
@@ -41,6 +41,7 @@ def get_fp_wells_from_clones(clones):
             select(Well)
             .join(Well.plates)
             .where(tuple_(Plate.name, Well.name).in_(clones))
+            .options(joinedload(Well.plates))
         )
 
         wells = [r[0] for r in results]
@@ -60,3 +61,5 @@ if __name__ == "__main__":
     fp_wells = get_fp_wells_from_clones(clones)
     # Should be one well for each clone.
     assert len(fp_wells) == expected_number_of_clones, f"Found {len(fp_wells)} freeze plate wells."
+    # Check that known clone, HUPFP0085A1_C10 from LIMS-46 tests, is in returned data.
+    assert len(list(filter(lambda well: well.name == "C10" and well.plates.name == "HUPFP0085A1", fp_wells))) == 1
