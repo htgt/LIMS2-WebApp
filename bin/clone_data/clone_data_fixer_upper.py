da8c05fd19dd3f7aabc4be41e9645c5fde7ea84c
ac64 <ac64@sanger.ac.uk>
2023-05-02 18:49:53 +0000

    Add assertion for correct miseq experiment
    Basic test that the miseq experiments are being added to the graph as
    expected.

--- bin/clone_data/clone_data_fixer_upper.py
+++ bin/clone_data/clone_data_fixer_upper.py
@@ -194,6 +194,21 @@ def assert_the_biggest_equivalence_class_has_graphs_of_the_expected_shape(equiva
     )
 
 
+def assert_miseq_experiment_correct_for_known_example(graphs):
+    # We test using the example HUPFP0085A1_C10 from 
+    # https://jira.sanger.ac.uk/browse/LIMS-46
+    for graph in graphs:
+        fp_well = get_fp_well_from_graph(graph)
+        if fp_well.plates.name == "HUPFP0085A1" and fp_well.name == "C10":
+            break
+    else:
+        assert False, "Can't find graph for clone HUPFP0085A1_C10"
+    miseq_experiments = get_miseq_experiments_from_graph(graph)
+    miseq_experiment_names = [me.name for me in miseq_experiments]
+    # Just check required miseq experiment is in list of all miseq experiments - others will exist.
+    assert "HUEDQ0591_BRPF1" in miseq_experiment_names, f"Found miseq experiment names: {miseq_experiment_names}"
+
+
 def plot_graphs(equivalence_classes):
     fig, axes = subplots(nrows=len(equivalence_classes), **{"figsize": (10, 50)})
     for equivalence_class, axis in zip(equivalence_classes, axes):
@@ -214,6 +229,11 @@ def get_fp_well_from_graph(graph):
 def get_piq_wells_from_graph(graph):
     piq_wells = [n for n, d in graph.nodes(data=True) if d["type"] == "piq_well"]
     return piq_wells
+
+
+def get_miseq_experiments_from_graph(graph):
+    miseq_experiments = [n for n, d in graph.nodes(data=True) if d["type"] == "miseq_experiment"]
+    return miseq_experiments
 
 
 def get_plate_names_from_graphs(graphs):
@@ -369,6 +389,8 @@ if __name__ == "__main__":
         add_miseq_wells_to_graph(graph)
         add_miseq_well_experiments_to_graph(graph)
         add_miseq_experiments_to_graph(graph)
+    assert_miseq_experiment_correct_for_known_example(graphs)
+
     equivalence_classes = create_equivalence_classes(
         [get_well_graph_from_graph(graph) for graph in graphs]
     )
