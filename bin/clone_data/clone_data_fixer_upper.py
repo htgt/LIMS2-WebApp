7e0d9fa6bb09a0ccfa0e80ea312108c7f699f850
ac64 <ac64@sanger.ac.uk>
2023-05-22 11:36:50 +0000

    Print out 'good' 2 piq/miseq cases

--- bin/clone_data/clone_data_fixer_upper.py
+++ bin/clone_data/clone_data_fixer_upper.py
@@ -587,7 +587,11 @@ if __name__ == "__main__":
     print_clone_data_results(results_from_checking)
 
     print("Good clones that previously had two PIQ/Miseq wells:")
-    good_clone_names = {result.clone_name for result in results_from_checking if result.error is None}
-    good_and_two_piq_miseq = good_clone_names & clone_names_for_graphs_with_two_piq_plates_and_two_miseq_wells 
-    print(good_and_two_piq_miseq)
+    good_and_two_piq_miseq = {
+        result for result in results_from_checking
+        if result.clone_name in clone_names_for_graphs_with_two_piq_plates_and_two_miseq_wells 
+    }
+    print("Clone name, Miseq experiment")
+    for result in good_and_two_piq_miseq:
+        print(result.clone_name + ", " + result.json_data["miseq_data"]["experiment_name"])
 
