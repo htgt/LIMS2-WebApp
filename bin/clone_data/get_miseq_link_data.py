67881a3023d56145decd03751b80eace5142d974
MrCurtis <4184070+MrCurtis@users.noreply.github.com>
2023-10-09 21:29:35 +0000

    Create readcount list

--- bin/clone_data/get_miseq_link_data.py
+++ bin/clone_data/get_miseq_link_data.py
@@ -5,7 +5,7 @@ from analyse_it import convert_alphanumeric_well_name_to_numeric as to_numeric
 init("lims2:team87@htgt-db.internal.sanger.ac.uk:5440/lims2_clone_data")
 from clone_data_fixer_upper import get_clones
 clones = get_clones(expected_number_of_clones=1866)
-def get_relevant_data(data):
+def get_miseq_related_data(data):
     return {
         "fp_plate": data["plate_name"],
         "fp_well": data["well_name"],
@@ -13,23 +13,49 @@ def get_relevant_data(data):
         "miseq_well": to_numeric(data["miseq"]["data"]["miseq_well"]),
         "miseq_experiment": data["miseq"]["data"]["experiment_name"]
     }
+
+def get_readcount_data(data):
+    return {
+        "fp_plate": data["plate_name"],
+        "fp_well": data["well_name"],
+        "total_reads": data["miseq"]["data"]["total_reads"],
+    }
     
 def filter_results_data_for_good_clones(results):
     return [
         result.json_data for result in results if result.error is None
     ]
     
-def transform_to_relevant_data(data):
+def transform_to_miseq_data(data):
     return [
-        get_relevant_data(datum) for datum in data
+        get_miseq_related_data(datum) for datum in data
+    ]
+
+def transform_to_read_count_data(data):
+    return [
+        get_readcount_data(datum) for datum in data
     ]
     
 def order_by_fp(data):
     return sorted(data, key=lambda d: (d["fp_plate"], d["fp_well"]))
     
+def order_by_total_reads(data):
+    return sorted(data, key=lambda d: d["total_reads"])
+    
 def get_all_relevant_good_clone_data():
     return order_by_fp(
-            transform_to_relevant_data(
+            transform_to_miseq_data(
+                filter_results_data_for_good_clones(
+                    check_clone_data(
+                        get_clones(expected_number_of_clones=1866)
+                    )
+                )
+            )
+        )
+
+def get_read_counts_for_all_good_clones():
+    return order_by_total_reads(
+            transform_to_read_count_data(
                 filter_results_data_for_good_clones(
                     check_clone_data(
                         get_clones(expected_number_of_clones=1866)
