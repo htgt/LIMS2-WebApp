42ab1352c79f8beb7eb84532120a1e55c4b54756
MrCurtis <4184070+MrCurtis@users.noreply.github.com>
2023-12-12 22:10:40 +0000

    Attempt to call function from script

--- bin/clone_data/classify-miseq-experiments.pl
+++ bin/clone_data/classify-miseq-experiments.pl
@@ -9,3 +9,7 @@ Add the classification for the clones in a miseq-well-experiment.
 use strict;
 use warnings;
 use feature qw(say);
+
+use LIMS2::Model::Util::Miseq qw/ classify_reads /;
+
+classify_reads();
