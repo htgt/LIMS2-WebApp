234b74dd32c5b49fbff7143504ee987a5652f7fd
MrCurtis <4184070+MrCurtis@users.noreply.github.com>
2023-12-14 12:42:24 +0000

    Get the allele_data

--- bin/clone_data/classify-miseq-experiments.pl
+++ bin/clone_data/classify-miseq-experiments.pl
@@ -12,7 +12,7 @@ use feature qw(say);
 use Getopt::Long;
 
 use LIMS2::Model;
-use LIMS2::Model::Util::Miseq qw/ classify_reads /;
+use LIMS2::Model::Util::Miseq qw/ classify_reads _get_miseq_data_from_well /;
 
 my $fp_plate_name = "";
 my $fp_well_name = "";
@@ -24,5 +24,8 @@ GetOptions(
 my $model = LIMS2::Model->new( user => 'lims2' );
 
 my $fp_well = $model->retrieve_well( { plate_name => $fp_plate_name, well_name => $fp_well_name } );
+my $miseq_data = _get_miseq_data_from_well($model, $fp_well);
+my $allele_data = $miseq_data->{data}->{allele_data};
 
-classify_reads();
+my $output = classify_reads($allele_data);
+say($output);
